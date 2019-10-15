e471ff51d60b83e8349af568b239e0cbc3240d53
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-10-15 17:25:20 +0300

    fix router

--- distribution/client/src/router.js
+++ distribution/client/src/router.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { hashHistory, IndexRoute, Route, Router, Switch } from "react-router";
-import { createBrowserHistory } from "history";
+import { createHashHistory } from "history";
 import App from "./containers/application/application";
 import Got from "./containers/got/got";
 import Funding from "./containers/funding/index";
 import Auction from "./containers/auction/index";
 import NotFound from "./containers/application/notFound";
 
-export const history = createBrowserHistory({});
+export const history = createHashHistory({});
 
 const AppRouter = () => (
     <Router history={history}>
