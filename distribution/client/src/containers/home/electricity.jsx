c12b3d2760495b11cbdee60c6e973c8208743358
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-10-30 15:09:34 +0300

    fix pages Home

--- distribution/client/src/containers/home/electricity.jsx
+++ distribution/client/src/containers/home/electricity.jsx
@@ -1,90 +1,87 @@
 import React from 'react';
+
 const cyb = require('../../image/logo-cyb-v2.svg');
 const cyber = require('../../image/cyber.png');
 
 export class Electricity extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            d: 'M0,100,500,100'
-        };
-        this.run();
-    }
-
-    update = () => {
-        let d = this.calculate(0, 0, 500, 80);
-        this.setState({
-            d
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      d: 'M0,100,500,100'
     };
+    this.run();
+  }
 
-    calculate = (x, y, width, height) => {
-        let points = [[x, height / 0.8]];
-        let maxPoints = 10;
-        let chunkRange = width / maxPoints;
-        for (let i = 0; i < maxPoints; i++) {
-            let cx = chunkRange * i + Math.cos(i) * chunkRange;
-            let cy = Math.random() * height*1.5;
-            points.push([cx, cy]);
-        }
+  update = () => {
+    const d = this.calculate(0, 0, 500, 80);
+    this.setState({
+      d
+    });
+  };
 
-        points.push([width, height/0.8]);
+  calculate = (x, y, width, height) => {
+    const points = [[x, height / 0.8]];
+    const maxPoints = 10;
+    const chunkRange = width / maxPoints;
+    for (let i = 0; i < maxPoints; i++) {
+      const cx = chunkRange * i + Math.cos(i) * chunkRange;
+      const cy = (Math.random() * height) / 0.8;
+      points.push([cx, cy]);
+    }
 
-        let d = points.map(point => point.join(','));
-        return 'M' + d.join(',');
+    points.push([width, height / 0.8]);
+
+    const d = points.map(point => point.join(','));
+    return `M${d.join(',')}`;
+  };
+
+  run() {
+    const fps = 30;
+    let now;
+    let delta;
+    let then = Date.now();
+    const interval = 1000 / fps;
+    let iteration = 0;
+    const loop = () => {
+      requestAnimationFrame(loop);
+
+      now = Date.now();
+      delta = now - then;
+      if (delta > interval) {
+        then = now - (delta % interval);
+
+        // update stuff
+        this.update(iteration++);
+      }
     };
+    loop();
+  }
 
-    run() {
-        let fps = 25,
-            now,
-            delta,
-            then = Date.now(),
-            interval = 1000 / fps,
-            iteration = 0,
-            loop = () => {
-                requestAnimationFrame(loop);
+  render() {
+    const { d } = this.state;
 
-                now = Date.now();
-                delta = now - then;
-                if (delta > interval) {
-                    then = now - (delta % interval);
-
-                    // update stuff
-                    this.update(iteration++);
-                }
-            };
-        loop();
-    }
-
-    render() {
-        const { d } = this.state;
-
-        return (
-            <div className="electricity">
-                <img style={{width: 100, height: 100}} src={cyber}></img>
-                <div className="line">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 200">
-                    <defs>
-                        <filter id="f1" x="0" y="0">
-                            <feGaussianBlur
-                                in="SourceGraphic"
-                                stdDeviation="5"
-                            />
-                        </filter>
-                    </defs>
-                    <g>
-                        <path
-                            d={d}
-                            fill="none"
-                            stroke="#3ab793"
-                            filter="url(#f1)"
-                        ></path>
-                        <path d={d} fill="none" stroke="#3ab793"></path>
-                    </g>
-                </svg>
-                </div>
-                <img style={{width: 100, height: 100}} src={cyb}></img>
-            </div>
-        );
-    }
+    return (
+      <div className="electricity">
+        <img style={{ width: 100, height: 100 }} src={cyber} />
+        <div className="line">
+          <svg
+            className="electricity-svg"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 500 200"
+          >
+            <defs>
+              <filter id="f1" x="0" y="0">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="5" />
+              </filter>
+            </defs>
+            <g>
+              <path d={d} fill="none" stroke="#3ab793" filter="url(#f1)" />
+              <path d={d} fill="none" stroke="#3ab793" />
+            </g>
+          </svg>
+        </div>
+        <img style={{ width: 100, height: 100 }} src={cyb} />
+      </div>
+    );
+  }
 }
--- distribution/client/src/containers/home/home.jsx
+++ distribution/client/src/containers/home/home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Button, Input, Pane } from '@cybercongress/ui';
 import { Electricity } from './electricity';
 
 const Home = () => (
-  <main className="block-body">
-    dfsdf
-    <Electricity />
+  <main className="block-body-home">
+    <Pane
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flex="auto"
+      d
+    >
+      <Input width="60%" placeholder="joint for validators" />
+    </Pane>
+    <Pane width="60%" marginY={0} marginX="auto">
+      <Electricity />
+    </Pane>
   </main>
 );
 
