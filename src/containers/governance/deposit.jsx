d895d8a4598af93c13ac5a1d669fb5108e9c5ecc
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-11-15 20:36:38 +0300

    Status Votes

---
+++ src/containers/governance/deposit.jsx
@@ -0,0 +1,16 @@
+const deposit = [
+  {
+    height: '104593',
+    result: {
+      min_deposit: [
+        {
+          denom: 'cyb',
+          amount: '500000000000',
+        },
+      ],
+      max_deposit_period: '7200000000000',
+    },
+  },
+];
+
+export default deposit;
--- src/containers/governance/governance.jsx
+++ src/containers/governance/governance.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pane, Text, TableEv as Table } from '@cybercongress/gravity';
+import { Pane, Text, TableEv as Table, Tooltip } from '@cybercongress/gravity';
 import { FormatNumber } from '../../components/index';
 import withWeb3 from '../../components/web3/withWeb3';
 // import { formatNumber } from '../../utils/search/utils';
@@ -11,6 +11,70 @@ const dateFormat = require('dateformat');
 const toFixedNumber = (number, toFixed) => {
   return Math.floor(number * 10 ** toFixed) / 10 ** toFixed;
 };
+
+const Legend = ({ color, text, ...props }) => (
+  <Pane display="flex" alignItems="center">
+    <Pane
+      width={12}
+      height={12}
+      borderRadius="2px"
+      display="inline-block"
+      marginRight={10}
+      backgroundColor={color}
+      verticalAlign="middle"
+      {...props}
+    />
+    <Text color="#fff" fontSize="16px">
+      {text}
+    </Text>
+  </Pane>
+);
+
+const Votes = ({ finalVotes }) => (
+  <Pane height={10} width="100%" display="flex">
+    <Tooltip
+      content={`Yes: ${toFixedNumber(finalVotes.yes, 2)}%`}
+      position="top"
+    >
+      <Pane
+        backgroundColor="#45b4ff"
+        display="flex"
+        height="100%"
+        width={`${finalVotes.yes}%`}
+      />
+    </Tooltip>
+    <Tooltip
+      content={`Abstain: ${toFixedNumber(finalVotes.abstain, 2)}%`}
+      position="top"
+    >
+      <Pane
+        backgroundColor="#ccdcff"
+        display="flex"
+        height="100%"
+        width={`${finalVotes.abstain}%`}
+      />
+    </Tooltip>
+    <Tooltip content={`No: ${toFixedNumber(finalVotes.no, 2)}%`} position="top">
+      <Pane
+        backgroundColor="#ffcf65"
+        display="flex"
+        height="100%"
+        width={`${finalVotes.no}%`}
+      />
+    </Tooltip>
+    <Tooltip
+      content={`NoWithVeto: ${toFixedNumber(finalVotes.no_with_veto, 2)}%`}
+      position="top"
+    >
+      <Pane
+        backgroundColor="#fe8a8a"
+        display="flex"
+        height="100%"
+        width={`${finalVotes.no_with_veto}%`}
+      />
+    </Tooltip>
+  </Pane>
+);
 
 class Governance extends React.Component {
   constructor(props) {
@@ -26,6 +90,22 @@ class Governance extends React.Component {
       table: proposals[0].result,
     });
   }
+
+  finalTallyResult = item => {
+    const finalVotes = {};
+    let finalTotalVotes = 0;
+    const yes = parseInt(item.yes);
+    const abstain = parseInt(item.abstain);
+    const no = parseInt(item.no);
+    const noWithVeto = parseInt(item.no_with_veto);
+
+    finalTotalVotes = yes + abstain + no + noWithVeto;
+    finalVotes.yes = (yes / finalTotalVotes) * 100;
+    finalVotes.no = (no / finalTotalVotes) * 100;
+    finalVotes.abstain = (abstain / finalTotalVotes) * 100;
+    finalVotes.no_with_veto = (noWithVeto / finalTotalVotes) * 100;
+    return finalVotes;
+  };
 
   render() {
     const { table } = this.state;
@@ -50,6 +130,7 @@ class Governance extends React.Component {
         </Table.TextCell>
         <Table.TextCell flex="none" width={150}>
           <Text color="#fff">{item.proposal_status}</Text>
+          <Votes finalVotes={this.finalTallyResult(item.final_tally_result)} />
         </Table.TextCell>
         <Table.TextCell flex="none" width={200}>
           <Text color="#fff">
@@ -86,69 +167,85 @@ class Governance extends React.Component {
     return (
       <main className="block-body-home">
         <Pane>
-          <Text color="#fff">{table.length} Proposals</Text>
-        </Pane>
-        <Pane
-          // height="100%"
-          display="flex"
-        >
-          <Table overflowX="auto">
-            <Table.Head
-              width="fit-content"
-              style={{
-                backgroundColor: '#000',
-                borderBottom: '1px solid #ffffff80',
-              }}
-              paddingLeft={20}
-            >
-              <Table.TextHeaderCell flex="none" width={50}>
-                <Text color="#fff" fontSize="17px">
-                  ID
-                </Text>
-              </Table.TextHeaderCell>
-              <Table.TextHeaderCell flex="none" width={150}>
-                <Text color="#fff" fontSize="17px">
-                  Title
-                </Text>
-              </Table.TextHeaderCell>
-              <Table.TextHeaderCell flex="none" width={170}>
-                <Text color="#fff" fontSize="17px">
-                  Type
-                </Text>
-              </Table.TextHeaderCell>
-              <Table.TextHeaderCell flex="none" width={150}>
-                <Text color="#fff" fontSize="17px">
-                  Status
-                </Text>
-              </Table.TextHeaderCell>
-              <Table.TextHeaderCell flex="none" width={200}>
-                <Text color="#fff" fontSize="17px">
-                  Submit_Time
-                </Text>
-              </Table.TextHeaderCell>
-              <Table.TextHeaderCell flex="none" width={200}>
-                <Text color="#fff" fontSize="17px">
-                  Deposit_Endtime
-                </Text>
-              </Table.TextHeaderCell>
-              <Table.TextHeaderCell flex="none" width={200}>
-                <Text color="#fff" fontSize="17px">
-                  Voting_Starttime
-                </Text>
-              </Table.TextHeaderCell>
-              <Table.TextHeaderCell flex="none" width={200}>
-                <Text color="#fff" fontSize="17px">
-                  Voting_Endtime
-                </Text>
-              </Table.TextHeaderCell>
-            </Table.Head>
-            <Table.Body
-              width="fit-content"
-              style={{ backgroundColor: '#000', overflow: 'hidden' }}
-            >
-              {rowsTable}
-            </Table.Body>
-          </Table>
+          <Pane
+            height={70}
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Text marginRight={50} marginLeft={20} color="#fff" fontSize="18px">
+              {table.length} Proposals
+            </Text>
+            <Pane display="flex">
+              <Legend color="#45b4ff" text="Yes" />
+              <Legend color="#ccdcff" text="Abstain" marginLeft={50} />
+              <Legend color="#ffcf65" text="No" marginLeft={50} />
+              <Legend color="#fe8a8a" text="NoWithVeto" marginLeft={50} />
+            </Pane>
+          </Pane>
+          <Pane
+            // height="100%"
+            display="flex"
+            paddingBottom={50}
+          >
+            <Table overflowX="auto">
+              <Table.Head
+                width="fit-content"
+                style={{
+                  backgroundColor: '#000',
+                  borderBottom: '1px solid #ffffff80',
+                }}
+                paddingLeft={20}
+              >
+                <Table.TextHeaderCell flex="none" width={50}>
+                  <Text color="#fff" fontSize="17px">
+                    ID
+                  </Text>
+                </Table.TextHeaderCell>
+                <Table.TextHeaderCell flex="none" width={150}>
+                  <Text color="#fff" fontSize="17px">
+                    Title
+                  </Text>
+                </Table.TextHeaderCell>
+                <Table.TextHeaderCell flex="none" width={170}>
+                  <Text color="#fff" fontSize="17px">
+                    Type
+                  </Text>
+                </Table.TextHeaderCell>
+                <Table.TextHeaderCell flex="none" width={150}>
+                  <Text color="#fff" fontSize="17px">
+                    Status
+                  </Text>
+                </Table.TextHeaderCell>
+                <Table.TextHeaderCell flex="none" width={200}>
+                  <Text color="#fff" fontSize="17px">
+                    Submit_Time
+                  </Text>
+                </Table.TextHeaderCell>
+                <Table.TextHeaderCell flex="none" width={200}>
+                  <Text color="#fff" fontSize="17px">
+                    Deposit_Endtime
+                  </Text>
+                </Table.TextHeaderCell>
+                <Table.TextHeaderCell flex="none" width={200}>
+                  <Text color="#fff" fontSize="17px">
+                    Voting_Starttime
+                  </Text>
+                </Table.TextHeaderCell>
+                <Table.TextHeaderCell flex="none" width={200}>
+                  <Text color="#fff" fontSize="17px">
+                    Voting_Endtime
+                  </Text>
+                </Table.TextHeaderCell>
+              </Table.Head>
+              <Table.Body
+                width="fit-content"
+                style={{ backgroundColor: '#000', overflow: 'hidden' }}
+              >
+                {rowsTable}
+              </Table.Body>
+            </Table>
+          </Pane>
         </Pane>
       </main>
     );
