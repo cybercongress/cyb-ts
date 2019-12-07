9875145667650ecfaa4c0efaf7d41064a6057795
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-12-08 01:55:29 +0300

    fix bug #88

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ const dev = process.env.NODE_ENV !== 'production';
 
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
   template: path.join(__dirname, 'src', 'index.html'),
+  favicon: 'src/image/favicon.ico',
   filename: 'index.html',
   inject: 'body',
 });
