b6fdfff3df757fb7303ebcbdada8516d92cc0c8f
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-12-20 17:22:53 +0300

    fix brain fix actionBar link

--- src/containers/brain/brain.jsx
+++ src/containers/brain/brain.jsx
@@ -98,7 +98,7 @@ class Brain extends React.Component {
       stakedCyb: 0,
       activeValidatorsCount: 0,
       selected: 'main',
-      loading: true,
+      loading: false,
       chainId: '',
       amount: 0,
       supplyEUL: 0,
@@ -480,7 +480,7 @@ class Brain extends React.Component {
           title={T.brain.cap}
           value={formatNumber(Math.floor(capATOM))}
         />
-        <CardStatisics
+        {/* <CardStatisics
           title={T.brain.supplyGOL}
           value={formatNumber(TOTAL_GOL_GENESIS_SUPPLY)}
         />
@@ -496,7 +496,7 @@ class Brain extends React.Component {
         <CardStatisics
           title={T.brain.capETH}
           value={formatNumber(Math.floor(capETH))}
-        />
+        /> */}
       </Pane>
     );
 
@@ -586,7 +586,14 @@ class Brain extends React.Component {
                 You do not have control over the brain. You need EUL tokens to
                 let she hear you. If you came from Ethereum or Cosmos you can
                 claim the gift of gods. Then start prepare to the greatest
-                tournament in universe: Game of Links.
+                tournament in universe:{' '}
+                <a
+                  target="_blank"
+                  href="https://cybercongress.ai/game-of-links/"
+                >
+                  Game of Links
+                </a>
+                .
               </Text>
             </Pane>
           )}
