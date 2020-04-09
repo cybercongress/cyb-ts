34b5a07939c111df084a39da02e251011fddbe77
dimakorzhovnik <korzhovnik.dima@gmail.com>
2020-04-09 11:06:29 +0300

    add account redux

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import ipfsReducer from './ipfs';
 import golReducer from './gol';
 import blockReducer from './block';
 import bandwidthReducer from './bandwidth';
+import accountReducer from './account';
 
 const rootReducer = combineReducers({
   ipfs: ipfsReducer,
   gol: golReducer,
   block: blockReducer,
   bandwidth: bandwidthReducer,
+  account: accountReducer,
 });
 
 export default rootReducer;
