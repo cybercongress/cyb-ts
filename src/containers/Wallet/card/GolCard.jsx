d80a5465458912a0fa413b6bd01c4bc138235622
serejandmyself <simanovskys@yandex.ru>
2020-04-21 19:29:57 +0200

    Update GolCard.jsx

--- src/containers/Wallet/card/GolCard.jsx
+++ src/containers/Wallet/card/GolCard.jsx
@@ -8,18 +8,19 @@ function GolCard({ pocket, ...props }) {
   return (
     <PocketCard {...props}>
       <Text fontSize="16px" color="#fff">
-        Welcome to the intergalactic tournament - Game of Links. GoL - is the
-        main preparation before{' '}
+        Welcome to the intergalactic tournament - Game of Links. GoL is the
+        main preparation stage before{' '}
         <Link to="/search/genesis">the main network launch</Link> of{' '}
         <LinkWindow to="https://ipfs.io/ipfs/QmceNpj6HfS81PcCaQXrFMQf7LR5FTLkdG9sbSRNy3UXoZ">
           the cyber protocol
         </LinkWindow>
-        . Main goal of the tournament is to collectively bootstrap{' '}
-        <Link to="/brain">Superintelligence</Link>. Everyone can find themselves
-        in this fascinating process: we need to setup physical infrastructure,
-        upload initial knowledge, and create a reserve for sustaining the
-        project during infancy. Athletes must solve different parts of the
-        puzzle and could win up to 10% of CYB in the Genesis.{' '}
+        . The main goal of the tournament is to collectively bootstrap the{' '}
+        <Link to="/brain">Superintelligence</Link>. Everyone can find
+              themselves in this fascinating process: we need to set up physical
+              infrastructure, upload the initial knowledge and create a reserve to
+              sustain the project during its infancy. Athletes need to solve
+              different parts of the puzzle and can win up to 10% of CYB in
+              the Genesis.{' '}
         <Link to="/gol">Go and play</Link>.
       </Text>
     </PocketCard>
