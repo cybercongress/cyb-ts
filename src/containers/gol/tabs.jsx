e16212c679edbfcb086b32e71391056dc9636e1c
serejandmyself <simanovskys@yandex.ru>
2020-04-21 19:10:40 +0200

    Update tabs.jsx

--- src/containers/gol/tabs.jsx
+++ src/containers/gol/tabs.jsx
@@ -19,11 +19,11 @@ export const Delegation = () => (
       Get more voting power for your validator - get more rewards!
     </Text>
     <Text lineHeight="24px" color="#fff" fontSize="18px">
-      This disciplines is social discipline with max prize of 5 TCYB. Huge chunk
-      of CYB stake allocated to all Ethereans and Cosmonauts. The more you
-      spread, the more users will claim its allocation, the more voting power as
-      validators you will have in Genesis. Details of reward calculation you can
-      find in{' '}
+      This disciplines is a social discipline with a max prize of 5 TCYB. A huge chunk
+      of the CYB stake is allocated to all Ethereans and Cosmonauts. The more you
+      spread the word, the more users will claim their allocation, the more voting power as
+      a validator you will have in the Genesis. You can find the details of the
+      reward calculations in the{' '}
       <a target="_blank" href="https://cybercongress.ai/game-of-links/">
         Game of Links rules
       </a>
@@ -43,13 +43,13 @@ export const Load = () => (
     width="100%"
   >
     <Text lineHeight="24px" marginBottom={20} color="#fff" fontSize="18px">
-      Submit as much cyberlinks as possible!
+      Submit as many cyberlinks as possible!
     </Text>
     <Text lineHeight="24px" color="#fff" fontSize="18px">
-      We need to test the network under heavy load. Testing of decentralized
-      networks under load near real conditions is hard and expensive. So we
-      invite you to submit as much cyberlinks as possible. Max reward for this
-      discipline is 6 TCYB. Details of reward calculation you can find in{' '}
+      We need to test the network under heavy load. The testing of decentralized
+      networks under load, near real conditions is difficult and expensive. We
+      invite you to submit as many cyberlinks as possible. The max reward for this
+      discipline is 6 TCYB. Details of the reward calculation can be found in the{' '}
       <a target="_blank" href="https://cybercongress.ai/game-of-links/">
         Game of Links rules
       </a>
@@ -69,15 +69,15 @@ export const Uptime = () => (
     width="100%"
   >
     <Text lineHeight="24px" marginBottom={20} color="#fff" fontSize="18px">
-      Setup you validator and get rewards for precommit counts!
+      Setup you validator and get rewards for precommits!
     </Text>
     <Text lineHeight="24px" color="#fff" fontSize="18px">
-      Max rewards for uptime is 2 TCYB.{' '}
+      The max rewards for the uptime discipline is 2 TCYB.{' '}
       <a
         target="_blank"
         href="https://cybercongress.ai/docs/cyberd/run_validator/"
       >
-        Run validator, become the hero!
+        Run a validator, become a hero!
       </a>
     </Text>
   </Pane>
@@ -94,18 +94,18 @@ export const FVS = () => (
     width="100%"
   >
     <Text lineHeight="24px" marginBottom={20} color="#fff" fontSize="18px">
-      Go on and convince your friend to become a hero!
+      Go on and convince your friends to become heroes!
     </Text>
     <Text lineHeight="24px" color="#fff" fontSize="18px">
-      Full Validator Set discipline. We want to bootstrap the cyber main network
-      with full validators set. So we assign group bonus to all validators for
-      self-organization. If the set of validators will increase over or is equal
-      to 100, and this number of validators can last for 10000 blocks, we will
+      Full Validator Set discipline. We want to bootstrap the mainnet of cyber 
+      with a full validators set. We have assigned a group bonus to all validators for
+      self-organization. If the set of validators will increase to over or is equal
+      to 100, and this number of validators will last for 10000 blocks, we will
       allocate an additional 2 TCYB to validators who take part in genesis. If
       the number of validators will increase to or over 146, under the same
-      conditions we will allocate an additional 3 TCYB. All rewards in that
-      discipline will be distributed to validators per capita. Details of reward
-      calculation you can find in{' '}
+      conditions we will allocate an additional 3 TCYB. All of the rewards for this
+      discipline will be distributed to validators per capita. Details of the reward
+      calculations may be seen in the{' '}
       <a target="_blank" href="https://cybercongress.ai/game-of-links/">
         Game of Links rules
       </a>
@@ -124,8 +124,8 @@ export const Euler4 = () => (
     width="100%"
   >
     <Text lineHeight="24px" color="#fff" fontSize="18px">
-      Oh! You miss the boat. This discipline is the reward for validators who
-      helped us test the network during 2019 year. Thank you for participation.
+      Oh! You missed the boat. This discipline is the reward for validators who
+      helped us to test the network during 2019. Thank you for participation.
     </Text>
   </Pane>
 );
@@ -145,8 +145,8 @@ export const Community = () => (
       Propose something that matters!
     </Text>
     <Text lineHeight="24px" color="#fff" fontSize="18px">
-      2 TEUL allocated to community pool in euler. All governance payouts will
-      be migrated to main network. That means that up to 2 TCYB can be allocated
+      2 TEUL are allocated to the community pool during the euler epoch. All governance payouts will
+      be migrated to the main network. This means that up to 2 TCYB can be allocated
       for community proposals during Game of Links.{' '}
       <a target="_blank" href="https://cybercongress.ai/game-of-links/">
         Details here
