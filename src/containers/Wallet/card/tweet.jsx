28d69af1d74f1d1508f271cf87bb83604da0c7d6
dimakorzhovnik <korzhovnik.dima@gmail.com>
2020-07-22 18:15:05 +0300

    add pocket news Today

--- src/containers/Wallet/card/tweet.jsx
+++ src/containers/Wallet/card/tweet.jsx
@@ -5,18 +5,95 @@ import { connect } from 'react-redux';
 import { PocketCard } from '../components';
 import { Copy, Dots, LinkWindow } from '../../../components';
 import { formatNumber } from '../../../utils/utils';
-import { getAvatar, getFollows, getTweet } from '../../../utils/search/utils';
+import {
+  getAvatar,
+  getFollows,
+  getTweet,
+  getContent,
+  getGraphQLQuery,
+} from '../../../utils/search/utils';
 import { setStageTweetActionBar } from '../../../redux/actions/pocket';
-import { POCKET } from '../../../utils/config';
+import { POCKET, PATTERN_CYBER } from '../../../utils/config';
 
 const FileType = require('file-type');
 const isSvg = require('is-svg');
+const dateFormat = require('dateformat');
 const img = require('../../../image/logo-cyb-v3.svg');
 
 const STAGE_ADD_AVATAR = 0;
 const STAGE_ADD_FIRST_FOLLOWER = 1;
 const STAGE_ADD_FIRST_TWEET = 2;
 const STAGE_READY = 3;
+
+const QueryCyberlink = (address, yesterday, time) =>
+  `query MyQuery {
+    cyberlink_aggregate(where: {_and: [{timestamp: {_gte: "${yesterday}"}}, {timestamp: {_lt: "${time}"}}, {object_from: {_eq: "QmbdH2WBamyKLPE5zu4mJ9v49qvY8BFfoumoVPMR5V4Rvx"}}, {subject: {_in: [${address}]}}]}) {
+      aggregate {
+        count
+      }
+    }
+  }`;
+
+const useNewsToday = account => {
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [follows, setFollows] = useState([]);
+
+  useEffect(() => {
+    const addressFollows = [];
+
+    if (account.match(PATTERN_CYBER)) {
+      const feachData = async () => {
+        const responseFollows = await getFollows(account);
+        if (responseFollows !== null && responseFollows.txs) {
+          for (const item of responseFollows.txs) {
+            const cid = item.tx.value.msg[0].value.links[0].to;
+            const addressResolve = await getContent(cid);
+            if (addressResolve) {
+              const addressFollow = addressResolve;
+              console.log('addressResolve :>> ', addressResolve);
+              if (addressFollow.match(PATTERN_CYBER)) {
+                addressFollows.push(`"${addressFollow}"`);
+              }
+            }
+          }
+          setFollows(addressFollows);
+        }
+      };
+      feachData();
+    }
+  }, [account]);
+
+  useEffect(() => {
+    if (follows.length > 0) {
+      feachDataCyberlink(follows);
+    }
+  }, [follows]);
+
+  const feachDataCyberlink = async followsProps => {
+    const d = new Date();
+    const time = dateFormat(d, 'yyyy-mm-dd');
+    const yesterday = dateFormat(
+      new Date(Date.parse(d) - 86400000),
+      'yyyy-mm-dd'
+    );
+    const response = await getGraphQLQuery(
+      QueryCyberlink(followsProps, yesterday, time)
+    );
+    if (
+      response.cyberlink_aggregate &&
+      response.cyberlink_aggregate.aggregate
+    ) {
+      setCount(response.cyberlink_aggregate.aggregate.count);
+      setLoading(false);
+    }
+  };
+
+  return {
+    count,
+    loading,
+  };
+};
 
 function TweetCard({
   account,
@@ -25,6 +102,9 @@ function TweetCard({
   node,
   ...props
 }) {
+  const { count: countNewsToday, loading: loadingNewsToday } = useNewsToday(
+    account
+  );
   const [stage, setStage] = useState(STAGE_ADD_AVATAR);
   const [loading, setLoading] = useState(true);
   const [avatar, setAvatar] = useState({ loading: true, img: null });
@@ -220,6 +300,23 @@ function TweetCard({
             alt="img-avatar"
             src={avatar.img !== null ? avatar.img : img}
           />
+        </Pane>
+        <Pane
+          marginX={10}
+          alignItems="center"
+          display="flex"
+          flexDirection="column"
+        >
+          <Pane fontSize="20px">
+            {loadingNewsToday ? (
+              <Dots />
+            ) : (
+              formatNumber(parseFloat(countNewsToday))
+            )}
+          </Pane>
+          <Pane>
+            <Link to="/brain">News today</Link>
+          </Pane>
         </Pane>
         <Pane
           marginX={10}
