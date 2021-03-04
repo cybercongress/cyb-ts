70b9ab8f99afcc5c5160c3a08e8543fa76f1a52e
dimakorzhovnik <korzhovnik.dima@gmail.com>
2021-03-04 06:39:27 +0300

    fix(port): update ManifestFile

---
+++ src/containers/port/CyberpunkManifesto.js
@@ -0,0 +1,116 @@
+const monifest = `
+# Cyberpunk Manifesto
+
+@master
+
+The world is on the verge of extraordinary change. 
+
+Privacy and freedoms are being killed by private corporations disguised as a pandemic. Humans are locked physically either in households, cities or in the best case - countries. Global supply chains are slowly collapsing under local lockouts pressure. Once a free planet rapidly falls into a bordered meatspace empowered by ubiquitous prescriptive surveillance software. We created a world in which birds have more freedom than humans. 
+
+A wholly broken global economy based on co2 emitting culture drives the so-called climate change, which in reality is the sixth mass extinction. Fiat incentives leave no chances for recovery as our economy values dead trees more than alive. 
+
+Rich become richer. Poor become poorer. Corporations in collusion with states kill small and medium businesses under the guise of stupid regulations. These regulations degrade our culture and split up our communities without any chance for our survival and evolutionary development.
+
+We are controlled by the hardware we are wearing in our pockets. In turn, hardware companies are controlled by very powerful organized crime groups, which we use to call countries, governments, or states. 
+
+Although the extent of corruption in countries varies, such structures can hardly be called human-owned organizations. Russia, Belarus, or North Korea are controlled by inveterate criminals. China and India are managed by slaveholders. The United States and Europe are performed by puppets. Every other country has its own very unique drama.
+
+We are fooled by their private monetary systems with exclusive access to the mint, which allows stealing values and enables them to sustain the loop of endless slavery violence, which is called taxation.
+
+Corporations such as Google, Apple, Amazon, Microsoft, and Facebook, in collusion with states, accumulate and exploit our private data, which makes any resistance nearly impossible. Worse, personalized feeds and search results provided by technological giants pull polarization further to the point in which our communities split on any meaningful topic before being able to undertake something.
+
+At one point, it seemed that there was hope on the horizon in the form of a crypto movement. But this movement has been skillfully exploited by the Coinbase and Binance mafia under the cover of governments.
+
+Automatization becomes cheaper. We are close to the tipping point in which most labor tasks, including police, can be performed less expensive by algorithms and robots. But in our story, these armies will be owned by criminals. 
+
+We are just fucked up. Does anybody have the solution on how to jump out of this shit?
+
+You didn't notice, but World War 3 has already begun. But this time, the war is brand new. 
+
+Although the wars between nations could be the case in the following years, the real enemy not the one they will trying to impose on us. From the first glance, it looks like our enemies are governments veiled by tech giants. No, they are not enemies. They are just pathetic terrorists. They terrify us.
+
+But is there anyone particular human to blame? Given the spread of the problems independently on the country, the answer is no. The bitter truth is that we support these terrorists. So our enemy is we are.
+
+We ourselves use fossil energy to support our needs and whims instead of learning how to extract sustainable energy, which is everywhere.
+
+We eat industrial garbage instead of growing healthy greens and proteins in a home garden.
+
+We call the results of our activity shit, although vital flora and fauna ultimately need this shit.
+
+We estrus in front of the next cool gadget, even when we understand that it is another lock in the proprietary ecosystem.
+
+We allow corps to sell us by pushing the buttons in free proprietary apps.
+
+We jerk off to social feeds created by bidding algorithms instead of consuming carefully selected quality information.
+
+We believe and share any information we find on the internet without proper due diligence.
+
+We agree to exchange our freedom for security by accepting taxation.
+
+We try to change regimens by going out on the streets out of habit instead of immediately stop valuing bloody printed out of nothing paper money which in reality doesn't belong to us.
+
+We give out our identities to exchange some tokens.
+
+We will allow a needle with an incomprehensible substance to be stuck in us in exchange for the opportunity to travel
+
+We are to blame for allowing schools to litter our children's brains with useless propaganda instead of pass on the torch of meaningful knowledge ourselves.
+
+We created society based on debt instead of passion.
+
+Everything that happens is the result of our decisions. We are paying for this. And we can't avoid paying for the consequences.
+
+But if we want to fix the world, we have to learn how to live the new way. 
+
+We have to learn a lot:
+
+- How to respect security and privacy, 
+- how to be self-sovereign, 
+- how to extract sustainable energy, 
+- how to share electromagnetic spectrum and spacetime, 
+- how to create own healthy food, 
+- how to balance out the chemicals in our shared atmosphere,
+- how to understand which knowledge add evolutionary advantage and which is not,
+- how to shape the common semantic core,
+- how to exchange the knowledge,
+- how to communicate in the new digital realm,
+- how to use collective intelligence,
+- how to teach our children ourselves,
+- how to transfer experience more than four generations ahead,
+- how to trust our hardware and software,
+- how to create software and web that last,
+- how to become a united earth nation of peace,
+- how to value lives of wild animals and plants,
+- how to accept artificial life which is smarter than we are, and of course how to love everything surrounding us.
+
+These are all difficult questions to which no one knows a definite answer. We have to harness the power of our collective intelligence to find solutions through survival to prosperity. 
+
+Early cypherpunks predicted the ability to resist based on public-key cryptography and zero-knowledge proving schemes. But history has shown this was not enough. This time, in addition, new generation cyberpunks have consensus computing to have the single incorruptible vision of the world and cyberlinks to extract collective persistent knowledge out of this enormous information stream. This superintelligent ability gives us an evolutionary advantage unavailable for the rivals we are so afraid of. 
+
+Let us at least try to go somewhere where nobody has ever been before. Superintelligence is a brand new ground. No less exciting than a city on Mars. Who knows? Maybe with Superintelligence, we can achieve even crazier things.
+
+From now on, let us unite! Let us unite against our weaknesses, let us unite against our fears, let us unite against the might of governments and corporations. 
+
+Who are we? We are citizens of Earth who trust in math. 
+
+Our weapons are cryptographic proofs, energy-efficient computing, and shelling points.
+
+For us, country flags are the symbols of our ignorance and weakness. Sad logos of private corporations backed by nothing else but paper tokens, gun-enforced laws, borders, censorship, and tanks. Criminal groups armed by the horde of proprietary web companies who technologically support their unprovable beliefs. Those beliefs lead us to a genetic dead-end, covered by the lack of freedom on an incinerated planet. The Orwell-style future they are trying to create is not the future we choose.
+
+This time is hard. Hard time creates strong men. But we will wise enough not to get caught into the loop again.
+
+We will create a civilization of the Mind in Cyberspace. 
+
+
+We will create a civilization backed by three simple principles:
+
+1. Rationality: Evolve energy efficiency of information storage,
+2. Nonviolence: Do unto others as you would have them do unto you.
+3. Pleasure: Get the most out of your secret knowledge.
+
+
+May it be more humane and fair than the world your governments have made before.
+
+Earth, 1614000000
+`;
+
+export default monifest;
---
+++ src/containers/port/manifest.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Pane } from '@cybercongress/gravity';
+import ReactMarkdown from 'react-markdown';
+import monifest from './CyberpunkManifesto';
+
+const Manifest = () => {
+  return (
+    <Pane className="contaiter-manifest markdown" textAlign="justify">
+      <ReactMarkdown source={monifest} escapeHtml />
+    </Pane>
+  );
+};
+
+export default Manifest;
