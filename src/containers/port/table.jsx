14e7775ff943aa7f0013f05e502b55d36df800a7
dimakorzhovnik <korzhovnik.dima@gmail.com>
2021-03-05 14:40:39 +0300

    fix(port): add table pinData

--- src/containers/port/table.jsx
+++ src/containers/port/table.jsx
@@ -26,16 +26,6 @@ const ItemSubGroup = ({ item, index }) => {
           {trimString(item.ethTxhash, 8, 8)}
         </a>
       </div>
-      {/* <div className="numberType display-none-mob">{item.block}</div> */}
-      {/* <div className="numberType">
-        {item.cyberHash === null ? (
-          DEFAULT_PROOF.toUpperCase()
-        ) : (
-          <Link to={`/network/euler/tx/${item.cyberHash.toUpperCase()}`}>
-            {trimString(item.cyberHash.toUpperCase(), 5, 5)}
-          </Link>
-        )}
-      </div> */}
       <Tooltip
         placement="bottom"
         tooltip={`${formatNumber(item.avPrice, 6)} ETH`} // item.price
@@ -74,7 +64,7 @@ const RowTable = ({ pin, address, item, mobile }) => {
   return (
     <RowTableTakeoff
       style={{ gridTemplateColumns: '1.9fr 0.5fr 0.5fr' }}
-      pinAddress={pin !== null ? address === pin.bech32 : false}
+      pinAddress={pin !== null ? address === pin : false}
       key={address}
       item={item.address.map((items, index) => (
         <ItemSubGroup item={items} index={index} />
@@ -110,7 +100,6 @@ const RowTable = ({ pin, address, item, mobile }) => {
 function Table({ data, onlyPin, pin, mobile, styles }) {
   const [dataTable, setDataTable] = useState({});
   let tableRow;
-  let tableRowPin;
 
   useEffect(() => {
     if (Object.keys(data).length > 0) {
@@ -139,20 +128,6 @@ function Table({ data, onlyPin, pin, mobile, styles }) {
     ));
   }
 
-  if (pin !== null) {
-    tableRowPin = Object.keys(dataTable)
-      .filter((dataFilter) => dataFilter === pin.bech32)
-      .map((key, index) => (
-        <RowTable
-          pin={pin}
-          address={key}
-          mobile={mobile}
-          key={key}
-          item={dataTable[key]}
-        />
-      ));
-  }
-
   return (
     <div style={styles} className="table">
       <div
@@ -160,25 +135,11 @@ function Table({ data, onlyPin, pin, mobile, styles }) {
         className="table-header-rows"
       >
         <div className="numberType address">Address (TX id)</div>
-        {/* <div className="numberType display-none-mob">Height</div> */}
-        {/* <div className="numberType">Proof TX</div> */}
         <div className="numberType">Price, ETH</div>
         <div className="numberType">ETH</div>
         <div className="numberType">GCYB</div>
       </div>
-      {pin !== null && onlyPin && (
-        <div
-          className="table-body"
-          style={{
-            marginBottom: 10,
-            paddingBottom: 10,
-            borderBottom: onlyPin ? 'none' : '1px solid #fff',
-          }}
-        >
-          {tableRowPin}
-        </div>
-      )}
-      {!onlyPin && <div className="table-body">{tableRow}</div>}
+      <div className="table-body">{tableRow}</div>
     </div>
   );
 }
