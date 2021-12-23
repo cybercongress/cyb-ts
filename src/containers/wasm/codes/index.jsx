c0687046938a8e159acf94b86ceff643289900d4
dimakorzhovnik <korzhovnik.dima@gmail.com>
2021-12-23 13:02:03 +0300

    fix(wasm): upload code

--- src/containers/wasm/codes/index.jsx
+++ src/containers/wasm/codes/index.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState, useContext } from 'react';
+import { connect } from 'react-redux';
 import { AppContext } from '../../../context';
 import Code from './code';
+import ActionBar from './actionBar';
+import useSetActiveAddress from '../../../hooks/useSetActiveAddress';
 
 import styles from './styles.scss';
 
-function Codes() {
+function Codes({ defaultAccount }) {
   const { jsCyber } = useContext(AppContext);
+  const { addressActive } = useSetActiveAddress(defaultAccount);
   const [codes, setCodes] = useState([]);
+  const [updateFnc, setUpdateFunc] = useState(0);
 
   useEffect(() => {
     const getCodes = async () => {
@@ -23,20 +28,32 @@ function Codes() {
       }
     };
     getCodes();
-  }, [jsCyber]);
+  }, [jsCyber, updateFnc]);
 
   console.log(`codes`, codes);
 
   return (
-    <main className="block-body">
-      <div className={styles.containerCodes}>
-        {codes.length > 0 &&
-          codes.map((item) => {
-            return <Code data={item} key={item.id} />;
-          })}
-      </div>
-    </main>
+    <>
+      <main className="block-body">
+        <div className={styles.containerCodes}>
+          {codes.length > 0 &&
+            codes.map((item) => {
+              return <Code data={item} key={item.id} />;
+            })}
+        </div>
+      </main>
+      <ActionBar
+        addressActive={addressActive}
+        updateFnc={() => setUpdateFunc((item) => item + 1)}
+      />
+    </>
   );
 }
 
-export default Codes;
+const mapStateToProps = (store) => {
+  return {
+    defaultAccount: store.pocket.defaultAccount,
+  };
+};
+
+export default connect(mapStateToProps)(Codes);
