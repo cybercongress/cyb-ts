b1426b855bba388583e19bb07dede16b637acd6e
dimakorzhovnik <korzhovnik.dima@gmail.com>
2022-01-04 13:24:14 +0300

    fix(wasm): useParseJsonSchema

--- src/containers/wasm/contract/renderAbi/useParseJsonSchema.js
+++ src/containers/wasm/contract/renderAbi/useParseJsonSchema.js
@@ -7,13 +7,21 @@ const useParseJsonSchema = (fileAbi) => {
     if (fileAbi !== null) {
       const objArr = [];
       const copyObjGlobal = { ...fileAbi };
-      delete copyObjGlobal.oneOf;
       delete copyObjGlobal.title;
       if (fileAbi.oneOf) {
+        delete copyObjGlobal.oneOf;
         Object.keys(fileAbi.oneOf).forEach((key) =>
           objArr.push({
             ...copyObjGlobal,
             ...fileAbi.oneOf[key],
+          })
+        );
+      } else if (fileAbi.allOf) {
+        delete copyObjGlobal.allOf;
+        Object.keys(fileAbi.allOf).forEach((key) =>
+          objArr.push({
+            ...copyObjGlobal,
+            ...fileAbi.allOf[key],
           })
         );
       } else {
