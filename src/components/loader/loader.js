e932bac35682f60920b9de3baedec106fa357f4c
Bloqhub <oleg@bloqhub.io>
2022-08-13 16:34:46 +0300

    Webpack & depends update to v5 Progressive preloader Website optimization & Lazy loading

---
+++ src/components/loader/loader.js
@@ -0,0 +1,240 @@
+require('./loader.css');
+
+const Bootloader = () => {
+  this.progressMap = {};
+  this.tagMap = {};
+  this.assets= {};
+  this.totalSize= 0;
+  this.networkSpeed= 0;
+
+
+  this.attachAssets = (assets) => {
+    this.assets = assets;
+    this.totalSize = this.getTotalSize(assets);
+    return this;
+  };
+  this.getTotalSize = (assets) => {
+    const cssAssets = assets.css || [];
+    const jsAssets = assets.js || [];
+    const sum = (acc, asset) => {
+      return acc + (asset.size || 0);
+    };
+    return cssAssets.reduce(sum, 0) + jsAssets.reduce(sum, 0);
+    return this;
+  };
+
+  this.updateProgress = (src, size) => {
+    if (size >= 0) {
+      const currentValue = this.progressMap[src] || 0;
+      if (size > currentValue) {
+        this.progressMap[src] = size;
+        return true;
+      }
+    }
+    return false;
+  };
+  this.getProgress = () => {
+    const _this = this;
+    return Object.keys(this.progressMap).reduce((acc, src) => {
+      return acc + _this.progressMap[src];
+    }, 0);
+  };
+  this.createScriptTag = (src, id) => {
+    const tag = document.createElement('script');
+    tag.id = id;
+    tag.type = 'text/javascript';
+    tag.src = src;
+    return tag;
+  };
+  this.createCssTag = (href, id) => {
+    const tag = document.createElement('link');
+    tag.id = id;
+    tag.rel = 'stylesheet';
+    tag.href = href;
+    return tag;
+  };
+  this.getBlob = (asset, cb) => {
+    const _this = this;
+    return new Promise((resolve, reject) => {
+      let endTime, fileSize;
+
+      let startTime = (new Date()).getTime();
+
+      const xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          endTime = (new Date()).getTime();
+          fileSize = xhr.response.size;
+          _this.networkSpeed = fileSize / ((endTime - startTime) / 1000) / 1024;
+          // Use (fileSize * 8) instead of fileSize for kBps instead of kbps
+          // Report the result, or have fries with it...
+          // console.log(speed + " kbps\n");
+        }
+      }
+
+      xhr.open('GET', asset.file, true);
+      xhr.responseType = 'blob';
+      // set listeners
+      xhr.addEventListener(
+        'error',
+        (err) => {
+          return reject(err);
+        },
+        false
+      );
+      xhr.addEventListener(
+        'progress',
+        (event) => {
+          if (event.lengthComputable) {
+            _this.updateProgress(asset.file, event.loaded) &&
+              cb({
+                totalSize: _this.totalSize,
+                networkSpeed: _this.networkSpeed,
+                loaded: _this.getProgress(),
+              });
+          }
+        },
+        false
+      );
+      xhr.addEventListener(
+        'load',
+        (event) => {
+          const { status } = xhr;
+          if (status === 200 || (status === 0 && xhr.response)) {
+            _this.updateProgress(asset.file, asset.size) &&
+              cb({
+                totalSize: _this.totalSize,
+                networkSpeed: _this.networkSpeed,
+                loaded: _this.getProgress(),
+              });
+            resolve(xhr.response);
+          } else {
+            reject('status: '.concat(xhr.status, ' - ').concat(xhr.statusText));
+          }
+        },
+        false
+      );
+      xhr.send();
+    });
+  };
+  this.loadAsset = (asset, js, cb) => {
+    const _this = this;
+    return this.getBlob(asset, cb).then((blob) => {
+      let _a;
+      const assetId = 'asset_'.concat(asset.file);
+      // remove asset if it exists
+      const oldAsset = document.getElementById(assetId);
+      oldAsset &&
+        ((_a = document.head) === null || _a === void 0
+          ? void 0
+          : _a.removeChild(oldAsset));
+      // create new asset
+      const objectURL = URL.createObjectURL(blob);
+      const tag = js
+        ? _this.createScriptTag(objectURL, assetId)
+        : _this.createCssTag(objectURL, assetId);
+      tag.onload = tag.onerror = () => {
+        // remove listeners
+        tag.onload = tag.onerror = null;
+        // note: if you want the file to be accessible after loading
+        // then comment out bellow line
+        URL.revokeObjectURL(objectURL);
+      };
+      _this.tagMap[asset.file] = tag;
+      return asset;
+    });
+  };
+  this.loadAssets = (assets, js, cb) => {
+    const _this = this;
+    const report = {
+      succeeded: [],
+      failed: [],
+    };
+    const tasks = assets.map((asset) => {
+      return _this
+        .loadAsset(asset, js, cb)
+        .then((done) => {
+          report.succeeded.push(done);
+        })
+        .catch((error) => {
+          report.failed.push({ asset, error });
+        });
+    });
+    return Promise.all(tasks).then(() => {
+      return report;
+    });
+  };
+  this.mergeReport = (lr1, lr2) => {
+    lr2.succeeded.forEach((i) => {
+      return lr1.succeeded.push(i);
+    });
+    lr2.failed.forEach((i) => {
+      return lr1.failed.push(i);
+    });
+  };
+  this.appendHtmlElements = (assets) => {
+    let _a;
+    for (let i = 0, assets_1 = assets; i < assets_1.length; i++) {
+      const asset = assets_1[i];
+      const tag = this.tagMap[asset.file];
+      if (tag) {
+        (_a = document.head) === null || _a === void 0
+          ? void 0
+          : _a.appendChild(tag);
+      }
+    }
+  };
+  this.load = (cb) => {
+    const _this = this;
+    const cssAssets = this.assets.css || [];
+    const jsAssets = this.assets.js || [];
+    const fullReport = {
+      succeeded: [],
+      failed: [],
+    };
+    return this.loadAssets(cssAssets, false, cb)
+      .then((report) => {
+        _this.mergeReport(fullReport, report);
+        _this.appendHtmlElements(cssAssets);
+        return _this.loadAssets(jsAssets, true, cb);
+      })
+      .then((report) => {
+        _this.mergeReport(fullReport, report);
+        _this.appendHtmlElements(jsAssets);
+        return fullReport;
+      });
+  };
+  return this;
+};
+
+function bootstrap() {
+  let _a;
+  const assets =
+    ((_a =
+      window === null || window === void 0 ? void 0 : window.$bootloader) ===
+      null || _a === void 0
+      ? void 0
+      : _a.assets) || {};
+  Bootloader()
+    .attachAssets(assets)
+    .load((e) => {
+      const progress = e.loaded / e.totalSize;
+      const progressBar = document.getElementById('progressbar');
+      const progressData = document.getElementById('progress-data');
+
+
+      progressBar === null || progressBar === void 0
+        ? void 0
+        : progressBar.setAttribute('value', progress.toString());
+
+        
+        progressData.innerHTML=`Loading: <span>${Math.round(progress*100)}%</span>. <br/> Network speed: <span>${Math.round(e.networkSpeed * 100) / 100}kbps</span>`;
+
+      // console.log(e.loaded, e.loaded / e.totalSize); // @TODO
+    })
+    .then((report) => {
+      //selfdestroy @TODO
+    });
+}
+exports.bootstrap = bootstrap;
+document.addEventListener('DOMContentLoaded', bootstrap);
--- src/router.jsx
+++ src/router.jsx
@@ -1,65 +1,87 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { Route, Router, Switch } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { connect } from 'react-redux';
 import { setTypeDevice } from './redux/actions/settings';
 import App from './containers/application/application';
-import SearchResults from './containers/Search/SearchResults';
-import Gift from './containers/gift';
-import Funding from './containers/funding/index';
-import Wallet from './containers/Wallet/Wallet';
-import Brain from './containers/brain/brain';
-import Home from './containers/home/home';
-import Governance from './containers/governance/governance';
-import ProposalsDetail from './containers/governance/proposalsDetail';
-import Validators from './containers/Validators/Validators';
-import Story from './containers/story/story';
-import GOL from './containers/gol/gol';
-import TxsDetails from './containers/txs/txsDetails';
-import AccountDetails from './containers/account';
-import ValidatorsDetails from './containers/validator';
-import Vesting from './containers/vesting/vesting';
-import Ipfs from './containers/ipfs/ipfs';
+
+
+const SearchResults = React.lazy(() => import('./containers/Search/SearchResults'));
+const Gift = React.lazy(() => import('./containers/gift'));
+const Funding = React.lazy(() => import('./containers/funding/index'));
+const Wallet = React.lazy(() => import('./containers/Wallet/Wallet'));
+const Brain = React.lazy(() => import('./containers/brain/brain'));
+const Home = React.lazy(() => import('./containers/home/home'));
+const Governance = React.lazy(() => import('./containers/governance/governance'));
+const ProposalsDetail = React.lazy(() => import('./containers/governance/proposalsDetail'));
+const Validators = React.lazy(() => import('./containers/Validators/Validators'));
+const Story = React.lazy(() => import('./containers/story/story'));
+const GOL = React.lazy(() => import('./containers/gol/gol'));
+const TxsDetails = React.lazy(() => import('./containers/txs/txsDetails'));
+const AccountDetails = React.lazy(() => import('./containers/account'));
+
+const ValidatorsDetails = React.lazy(() => import('./containers/validator'));
+const Vesting = React.lazy(() => import('./containers/vesting/vesting'));
+
+
+const Ipfs = React.lazy(() => import('./containers/ipfs/ipfs'));
+
 import { Dots, Timer } from './components';
 import { initIpfs, setIpfsStatus, setIpfsID } from './redux/actions/ipfs';
 import { setBlock } from './redux/actions/block';
-import BlockDetails from './containers/blok/blockDetails';
-import Txs from './containers/txs';
-import Block from './containers/blok';
-import ParamNetwork from './containers/parameters';
-import Evangelism from './containers/evangelism';
-import Got from './containers/got/got';
-import TrollBoxx from './containers/trollBox';
+
+const BlockDetails  = React.lazy(() => import('./containers/blok/blockDetails'));
+const Txs  = React.lazy(() => import('./containers/txs'));
+const Block  = React.lazy(() => import('./containers/blok'));
+const ParamNetwork  = React.lazy(() => import('./containers/parameters'));
+const Evangelism  = React.lazy(() => import('./containers/evangelism'));
+const Got  = React.lazy(() => import('./containers/got/got'));
+const TrollBoxx  = React.lazy(() => import('./containers/trollBox'));
+
 import useIpfsStart from './ipfsHook';
-import ForceGraph from './containers/forceGraph/forceGraph';
-import ForceQuitter from './containers/forceGraph/forceQuitter';
-import PortPages from './containers/port';
-import TestKeplr from './containers/testKeplre';
-import Mint from './containers/mint';
-import RoutedEnergy from './containers/energy';
-import Bootcycle from './containers/bootcycle';
-import Market from './containers/market';
-import Oracle from './containers/oracle';
-import Objects from './containers/Objects';
-import Taverna from './containers/taverna';
-import Teleport from './containers/teleport';
-import Nebula from './containers/nebula';
-import Genesis from './containers/genesis';
-import Movie from './containers/movie';
-import PortalCitizenship from './containers/portal';
-import PortalGift from './containers/portal/gift';
-import Release from './containers/portal/release';
-import Ibc from './containers/ibc';
-import {
-  Codes,
+
+
+
+const TestKeplr = React.lazy(() => import('./containers/testKeplre'));
+const ForceGraph = React.lazy(() => import('./containers/forceGraph/forceGraph'));
+const ForceQuitter = React.lazy(() => import('./containers/forceGraph/forceQuitter'));
+const PortPages = React.lazy(() => import('./containers/port'));
+
+const Mint = React.lazy(() => import('./containers/mint'));
+const RoutedEnergy  = React.lazy(() => import('./containers/energy'));
+const Bootcycle  = React.lazy(() => import('./containers/bootcycle'));
+
+const Market  = React.lazy(() => import('./containers/market'));
+const Oracle  = React.lazy(() => import('./containers/oracle'));
+const Objects  = React.lazy(() => import('./containers/Objects'));
+const Taverna  = React.lazy(() => import('./containers/taverna'));
+const Teleport  = React.lazy(() => import('./containers/teleport'));
+const Nebula  = React.lazy(() => import('./containers/nebula'));
+const Genesis  = React.lazy(() => import('./containers/genesis'));
+const Movie  = React.lazy(() => import('./containers/movie'));
+const PortalCitizenship  = React.lazy(() => import('./containers/portal'));
+const PortalGift  = React.lazy(() => import('./containers/gift'));
+
+const Release  = React.lazy(() => import('./containers/portal/release'));
+const Ibc  = React.lazy(() => import('./containers/ibc'));
+
+const {Codes,
   CodePage,
   ContractPage,
-  DashboardPage,
-} from './containers/wasm';
-import Help from './containers/help';
-import Assets from './containers/assets';
-import MainPartal from './containers/portal/mainPortal';
+  DashboardPage}  = React.lazy(() => import('./containers/wasm'));
+
+// import {
+//   Codes,
+//   CodePage,
+//   ContractPage,
+//   DashboardPage,
+// } from './containers/wasm';
+
+
+const Help  = React.lazy(() => import('./containers/help'));
+const Assets  = React.lazy(() => import('./containers/assets'));
+const MainPartal  = React.lazy(() => import('./containers/portal/mainPortal'));
 
 import useIpfsFactory from './useIpfsFactory';
 
@@ -92,6 +114,7 @@ function AppRouter({
 
   return (
     <Router history={history}>
+      <Suspense fallback={<div>Loading...</div>}>
       <Route path="/" component={App} />
       <Switch>
         <Route path="/" exact component={Wallet} />
@@ -156,6 +179,7 @@ function AppRouter({
         <Route path="/help" component={Help} />
         <Route path="/assets" component={Assets} />
       </Switch>
+      </Suspense>
     </Router>
   );
 }
