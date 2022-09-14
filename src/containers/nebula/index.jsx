a67d9396d12981dc1aca5415d7e78983ba2d3ccf
dimakorzhovnik <korzhovnik.dima@gmail.com>
2022-09-14 22:37:17 +0300

    fix(packege): update packege

--- src/containers/nebula/index.jsx
+++ src/containers/nebula/index.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState, useMemo } from 'react';
 import BigNumber from 'bignumber.js';
 import { v4 as uuidv4 } from 'uuid';
 import { connect } from 'react-redux';
-import { useWorker } from '@koale/useworker';
 import { Link } from 'react-router-dom';
 import {
   MainContainer,
@@ -42,56 +41,6 @@ const getTypeDenomKey = (denom) => {
 
   return denom;
 };
-
-function getMarketData() {
-  const getTokenIndexer = (wtl) => {
-    const tokenIndexer = {};
-    if (wtl) {
-      wtl.forEach((item) => {
-        tokenIndexer[item.denom] = item.amount;
-      });
-    }
-    return tokenIndexer;
-  };
-  return new Promise((resolve) =>
-    fetch('https://lcd.bostrom.cybernode.ai//cosmos/liquidity/v1beta1/pools')
-      .then((response) => response.json())
-      .then((data) => data.pools)
-      .then((responseDataPools) => {
-        const copyObjTemp = [];
-
-        // console.log('responseDataPools', responseDataPools);
-        if (responseDataPools && Object.keys(responseDataPools).length > 0) {
-          // eslint-disable-next-line no-restricted-syntax
-          for (const key in responseDataPools) {
-            if (Object.hasOwnProperty.call(responseDataPools, key)) {
-              const element = responseDataPools[key];
-              const { reserve_account_address: reserveAccountAddress } =
-                element;
-              // eslint-disable-next-line no-await-in-loop
-              fetch(
-                `https://lcd.bostrom.cybernode.ai/bank/balances/${reserveAccountAddress}`
-              )
-                .then((response) => response.json())
-                .then((data) => data.result)
-                .then((dataBalance) => {
-                  // console.log('dataBalance', dataBalance);
-                  element.balances = getTokenIndexer(dataBalance);
-                  copyObjTemp.push(element);
-                });
-            }
-          }
-          console.log('copyObjTemp', copyObjTemp);
-          if (copyObjTemp.length > 0) {
-            console.log('copyObjTemp', copyObjTemp);
-            resolve(responseDataPools);
-          }
-        }
-        // console.log('responseDataPools', responseDataPools)
-      })
-      .catch((e) => [])
-  );
-}
 
 function Nebula({ node, mobile, defaultAccount }) {
   const { dataTotal, marketData } = useGetMarketData();
