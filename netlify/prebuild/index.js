56507209cef903071804568484a22dca604419a8
Cheslav Zhuravsky <happylolonly@gmail.com>
2023-05-15 16:09:20 +0400

    fix(fix): fix

--- netlify/prebuild/index.js
+++ netlify/prebuild/index.js
@@ -1,10 +1,3 @@
 module.exports.onPreBuild = function ({ netlifyConfig }) {
-  console.log('Hello world from onPreBuild event!');
-
-  console.log(netlifyConfig);
-
-  console.log(process.env);
-  console.log(process.env.COMMIT_REF);
-
   netlifyConfig.build.environment.COMMIT_SHA = process.env.COMMIT_REF;
 };
