dc27c892f2e9d6734fd407539d15e18cb3029201
Cheslav Zhuravsky <happylolonly@gmail.com>
2023-05-25 21:49:11 +0400

    feat(robot): update

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,33 +1,58 @@
-import { Column, useTable } from 'react-table';
+import {
+  useReactTable,
+  Column,
+  ColumnDef,
+  getCoreRowModel,
+  flexRender,
+} from '@tanstack/react-table';
 
 import styles from './Table.module.scss';
 import Loader2 from '../ui/Loader2';
 import NoItems from '../ui/noItems';
 
-type Row = string[];
-
-export type Props = {
-  columns: Column<Row>[];
-  data: Row[];
+export type Props<T extends object> = {
+  columns: ColumnDef<T>[];
+  data: T[];
   isLoading?: boolean;
+  onSelect?: (id: number) => void;
 };
 
-function Table({ columns, data, isLoading }: Props) {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data });
+function Table<T extends object>({
+  columns,
+  data,
+  isLoading,
+  onSelect,
+}: Props<T>) {
+  const table = useReactTable({
+    columns,
+    data,
+    state: {
+      rowSelection: {},
+    },
+    debugTable: true,
+    enableRowSelection: true,
+    onRowSelectionChange: (state) => {
+      console.log(state);
+      debugger;
+    },
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  console.log(table);
 
   return (
     <>
-      <table {...getTableProps()} className={styles.table}>
+      <table className={styles.table}>
         <thead>
-          {headerGroups.map((headerGroup) => (
-            // eslint-disable-next-line react/jsx-key
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => {
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
                 return (
-                  // eslint-disable-next-line react/jsx-key
-                  <th {...column.getHeaderProps()}>
-                    {column.render('Header')}
+                  <th key={header.id}>
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
                   </th>
                 );
               })}
@@ -35,16 +60,30 @@ function Table({ columns, data, isLoading }: Props) {
           ))}
         </thead>
 
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
+        <tbody>
+          {table.getRowModel().rows.map((row) => {
             return (
-              // eslint-disable-next-line react/jsx-key
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
+              <tr
+                key={row.id}
+                data-id={row.id}
+                // style={{
+                //   color: row.getIsSelected() ? 'red' : 'black',
+                // }}
+                // onClick={(e) => {
+                //   // TODO: move to tbody
+                //   const id = Number(e.currentTarget.getAttribute('data-id'));
+                //   onSelect?.(id);
+                // }}
+              >
+                {row.getVisibleCells().map((cell) => {
                   return (
                     // eslint-disable-next-line react/jsx-key
-                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
                   );
                 })}
               </tr>
