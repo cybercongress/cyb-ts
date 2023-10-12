7e49985cfd352eae322f762fcfb8a96f9a1d6c6e
Cheslav Zhuravsky <happylolonly@gmail.com>
2023-10-12 22:50:22 +0400

    update

--- src/pages/Search/Learn/Learn.tsx
+++ src/pages/Search/Learn/Learn.tsx
@@ -1,13 +1,96 @@
-import { Link } from 'react-router-dom';
-import { ActionBar, Button, Input } from 'src/components';
+import { Link, useNavigate } from 'react-router-dom';
+import { ActionBar, Button, Input, MainContainer } from 'src/components';
 import { routes } from 'src/routes';
 import TitleText from '../TitleText/TitleText';
+import styles from './Learn.module.scss';
+import KeywordButton from '../KeywordButton/KeywordButton';
+import { useEffect, useState } from 'react';
+import { DEFAULT_GAS_LIMITS, PATTERN_IPFS_HASH } from 'src/utils/config';
+import { addContenToIpfs } from 'src/utils/ipfs/utils-ipfs';
+import { useIpfs } from 'src/contexts/ipfs';
+import { useAdviser } from 'src/features/adviser/context';
+import Loading from '../../../components/ui/Loading';
+import { useQueryClient } from 'src/contexts/queryClient';
+import { selectCurrentAddress } from 'src/redux/features/pocket';
+import { useSelector } from 'react-redux';
+import { useSigningClient } from 'src/contexts/signerClient';
+import useWaitForTransaction from 'src/hooks/useWaitForTransaction';
 
 function Learn() {
+  const [ask, setAsk] = useState('');
+  const [answer, setAnswer] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+  const { node } = useIpfs();
+  const queryClient = useQueryClient();
+
+  const { signer, signingClient } = useSigningClient();
+  const navigate = useNavigate();
+
+  const [tx, setTx] = useState({
+    hash: '',
+    onSuccess: () => {},
+  });
+
+  useWaitForTransaction({
+    hash: tx.hash,
+    onSuccess: tx.onSuccess,
+  });
+
+  const address = useSelector(selectCurrentAddress);
+
+  const { setAdviser } = useAdviser();
+
+  useEffect(() => {
+    // setAdviser(loading ? 'loading...' : '', loading ? 'yellow' : 'red');
+  }, [loading, setAdviser]);
+
+  async function cyberlink() {
+    if (!node || !queryClient || !address || !signingClient) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      let fromCid = ask;
+      if (!ask.match(PATTERN_IPFS_HASH)) {
+        fromCid = await addContenToIpfs(node, ask);
+      }
+
+      let toCid = answer;
+      if (!answer.match(PATTERN_IPFS_HASH)) {
+        toCid = await addContenToIpfs(node, answer);
+      }
+
+      const result = await signingClient.cyberlink(address, fromCid, toCid, {
+        amount: [],
+        gas: DEFAULT_GAS_LIMITS.toString(),
+      });
+
+      if (result.code !== 0) {
+        throw new Error(result.rawLog);
+      }
+
+      setTx({
+        hash: result.transactionHash,
+        onSuccess: () => {
+          debugger;
+          navigate(routes.ipfs.getLink(toCid));
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      setAdviser(error.message, 'red');
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <div>
-      <div>
-        <h6>linking is fundamental to learning</h6>
+    <MainContainer width="100%">
+      <div className={styles.info}>
+        <h3 className={styles.titleText}>linking is fundamental to learning</h3>
         <p>
           learning can be squeezed into one simple act: creating a link when you
           link two{' '}
@@ -25,7 +108,7 @@ function Learn() {
         </p>
       </div>
 
-      <ul>
+      <ul className={styles.list}>
         {[
           {
             title: 'upgrade',
@@ -48,35 +131,59 @@ function Learn() {
         })}
       </ul>
 
-      <div>
-        <h5>here is some cool links</h5>
+      <div className={styles.linksExample}>
+        <h3 className={styles.titleText}>here is some cool links</h3>
 
-        {[
-          ['health', 'meditation'],
-          ['music', 'joy'],
-          ['cyber', 'freedom'],
-          ['help', 'get H'],
-          ['help', 'get energy'],
-        ].map(([title, text]) => {
-          return (
-            <li key={title}>
-              <TitleText title={title} text={text} />
-            </li>
-          );
-        })}
+        <ul>
+          {[
+            ['health', 'meditation'],
+            ['music', 'joy'],
+            ['cyber', 'freedom'],
+            ['help', 'get H'],
+            ['help', 'get energy'],
+          ].map(([word1, word2], i) => {
+            return (
+              <li key={i}>
+                <KeywordButton keyword={word1} />
+                <span className={styles.tilde}>~</span>
+                <KeywordButton keyword={word2} />
+              </li>
+            );
+          })}
+        </ul>
       </div>
 
-      <div>
-        <h5>now learn something new</h5>
+      <section className={styles.learn}>
+        <h3 className={styles.titleText}>now learn something new</h3>
 
-        <Input />
-        <Input />
-      </div>
+        <div className={styles.inputs}>
+          <Input
+            color="pink"
+            placeholder="Ask"
+            value={ask}
+            disabled={loading}
+            onChange={(e) => setAsk(e.target.value)}
+          />
+          <span className={styles.tilde}>~</span>
+          <Input
+            color="pink"
+            disabled={loading}
+            value={answer}
+            placeholder="Answer"
+            onChange={(e) => setAnswer(e.target.value)}
+          />
+        </div>
+      </section>
 
       <ActionBar>
-        <Button>learn</Button>
+        <Button
+          disabled={!ask || !answer || !node || !queryClient}
+          onClick={cyberlink}
+        >
+          cyberlink
+        </Button>
       </ActionBar>
-    </div>
+    </MainContainer>
   );
 }
 
