b6d8b8b39d45fcf86628a7f37e160d0ea5972f7b
master <starodubcev@gmail.com>
2023-11-21 11:26:27 +0000

    fix text

--- src/containers/teleport/pages/mainScreen/components/TeleportText/TeleportText.tsx
+++ src/containers/teleport/pages/mainScreen/components/TeleportText/TeleportText.tsx
@@ -7,8 +7,6 @@ function TeleportText() {
         <h2>communicate</h2> <span>by exchanging value</span>
       </header>
       <p className={styles.description}>
-        safely exchange value in first web 3 powered app, send public texts,
-        check historical data with your contacts in timeline and many more
       </p>
     </div>
   );
