4f1e5d414e08e3d5a2d1e65c91f7a5bd2cb6d15a
Cheslav Zhuravsky <happylolonly@gmail.com>
2023-11-28 14:06:19 +0400

    refactor stats

--- src/pages/Search/Stats/Stats.tsx
+++ src/pages/Search/Stats/Stats.tsx
@@ -4,9 +4,6 @@ import {
 } from 'src/containers/temple/hooks';
 import { TypingText } from 'src/containers/temple/pages/play/PlayBanerContent';
 import cx from 'classnames';
-import { useQuery } from 'react-apollo';
-import gql from 'graphql-tag';
-import landingStyles from '../Search.module.scss';
 import styles from './Stats.module.scss';
 import { TitleType } from '../Search';
 
@@ -14,51 +11,24 @@ type Props = {
   type: TitleType;
 };
 
-const twentyFourHoursAgo = new Date(
-  new Date().getTime() - 24 * 60 * 60 * 1000
-).toISOString();
-
-function generateQuery(type: string) {
-  return gql`
-    query Query {
-      ${type}(where: {timestamp: {_gte: "${twentyFourHoursAgo}"}}) {
-        aggregate {
-          count
-        }
-      }
-    }
-  `;
-}
-
 const REFETCH_INTERVAL = 1000 * 7;
 
 function Stats({ type }: Props) {
   const dataGetGraphStats = useGetGraphStats(REFETCH_INTERVAL);
   const negentropy = useGetNegentropy(REFETCH_INTERVAL);
 
-  // const cyberlinksQuery = useQuery(generateQuery('cyberlinks_aggregate'));
-  // const particlesQuery = useQuery(generateQuery('particles_aggregate'));
-
   let value: number | undefined;
   let text: string;
-  let change: number | undefined;
 
   switch (type) {
     case TitleType.search:
       value = dataGetGraphStats.data?.particles;
       text = 'particles';
-
-      // if (!(particlesQuery.loading || particlesQuery.error)) {
-      //   change = particlesQuery.data?.particles_aggregate.aggregate.count;
-      // }
       break;
 
     case TitleType.learning:
       value = dataGetGraphStats.data?.cyberlinks;
       text = 'cyberlinks';
-      // if (!(cyberlinksQuery.loading || cyberlinksQuery.error)) {
-      //   change = cyberlinksQuery.data?.cyberlinks_aggregate.aggregate.count;
-      // }
       break;
 
     case TitleType.ai:
