80cb947d6f996f21ad4517d52f13906cf027171e
Cheslav Zhuravsky <happylolonly@gmail.com>
2023-12-01 13:56:01 +0400

    add author hovers

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -245,8 +245,21 @@ function Search() {
       {/* </ul> */}
 
       <div className={styles.footer}>
-        {['cyber', 'donut of knowledge', 'help'].map((keyword) => {
-          return <KeywordButton key={keyword} keyword={keyword} />;
+        {[
+          {
+            query: 'cyber',
+            author: 'bostrom1d8754xqa9245pctlfcyv8eah468neqzn3a0y0t',
+          },
+          {
+            query: 'donut of knowledge',
+            author: 'bostrom1k7nssnnvxezpp4una7lvk6j53895vadpqe6jh6',
+          },
+          {
+            query: 'help',
+            author: 'bostrom1hmkqhy8ygl6tnl5g8tc503rwrmmrkjcq3lduwj',
+          },
+        ].map(({ query, author }) => {
+          return <KeywordButton key={query} keyword={query} author={author} />;
         })}
       </div>
 
--- src/pages/Search/components/KeywordButton/KeywordButton.tsx
+++ src/pages/Search/components/KeywordButton/KeywordButton.tsx
@@ -1,16 +1,20 @@
 import { Link } from 'react-router-dom';
 import styles from './KeywordButton.module.scss';
 import { routes } from 'src/routes';
+import { Account, Tooltip } from 'src/components';
+import Pill from 'src/components/Pill/Pill';
 
-function KeywordButton({ keyword }: { keyword: string }) {
+function KeywordButton({ keyword, author }: { keyword: string }) {
   return (
-    <Link
-      className={styles.keywordBtn}
-      key={keyword}
-      to={routes.search.getLink(keyword)}
-    >
-      {keyword}
-    </Link>
+    <Tooltip tooltip={author && <Account avatar address={author} />}>
+      <Link
+        className={styles.keywordBtn}
+        key={keyword}
+        to={routes.search.getLink(keyword)}
+      >
+        {keyword}
+      </Link>
+    </Tooltip>
   );
 }
 
