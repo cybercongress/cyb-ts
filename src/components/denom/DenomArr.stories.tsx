a9bdf9c08d7e944404a2141b217febedb4163711
Cheslav Zhuravsky <happylolonly@gmail.com>
2023-12-12 20:07:21 +0400

    update Denom components

---
+++ src/components/denom/DenomArr.stories.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-unused-modules */
+
+import { Meta, StoryObj } from '@storybook/react';
+
+import DenomArr from './denomArr';
+
+const meta: Meta<typeof DenomArr> = {
+  component: DenomArr,
+  title: 'atoms/DenomArr',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: '',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof DenomArr>;
+type DenomArrProps = React.ComponentProps<typeof DenomArr>;
+
+const defaultArgs: DenomArrProps = {
+  denomValue: 'hydrogen',
+};
+
+export const Main: Story = {
+  args: defaultArgs,
+};
--- src/components/denom/denomArr.tsx
+++ src/components/denom/denomArr.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { TraseDenomFuncResponse } from 'src/types/ibc';
 import { Option } from 'src/types';
 import { useIbcDenom } from 'src/contexts/ibcDenom';
@@ -25,111 +25,105 @@ function DenomArr({
 }: DenomArrProps) {
   const { traseDenom } = useIbcDenom();
 
-  const useDenomValue = useMemo(() => {
-    let denom: Option<TraseDenomFuncResponse[]>;
+  const [denom, setDenom] = useState<TraseDenomFuncResponse[]>();
 
-    if (type === undefined) {
-      const denomTrase = traseDenom(denomValue);
-      denom = denomTrase;
+  useEffect(() => {
+    if (type) {
+      return;
     }
+    const denomTrase = traseDenom(denomValue);
 
-    if (type === 'network') {
-      return (
-        <Denom
-          onlyImg={onlyImg}
-          onlyText={onlyText}
-          denomValue={denomValue}
-          tooltipStatusImg={tooltipStatusImg}
-          tooltipStatusText={tooltipStatusText}
-          type={type}
-          infoDenom={{}}
-          gap={13}
-          size={size}
-        />
-      );
-    }
+    setDenom(denomTrase);
+  }, [denomValue, traseDenom, type]);
 
-    if (denom && denom.length === 1) {
-      return (
-        <Denom
-          onlyImg={onlyImg}
-          onlyText={onlyText}
-          denomValue={denom[0].denom}
-          tooltipStatusImg={tooltipStatusImg}
-          tooltipStatusText={tooltipStatusText}
-          type={type}
-          infoDenom={denom[0]}
-          gap={13}
-          size={size}
-        />
-      );
-    }
-
-    if (denom && denom.length > 1) {
-      return (
-        <div style={{ display: 'flex' }}>
-          {!onlyText && (
-            <>
-              <Denom
-                type={type}
-                denomValue={denom[0].denom}
-                onlyImg
-                tooltipStatusImg={tooltipStatusImg}
-                tooltipStatusText={tooltipStatusText}
-                infoDenom={denom[0]}
-                size={size}
-              />
-              <Denom
-                type={type}
-                denomValue={denom[1].denom}
-                marginContainer={
-                  onlyImg ? '0px 0px 0px -12px' : '0px 5px 0px -12px'
-                }
-                onlyImg
-                tooltipStatusImg={tooltipStatusImg}
-                tooltipStatusText={tooltipStatusText}
-                infoDenom={denom[1]}
-                size={size}
-              />
-            </>
-          )}
-
-          {!onlyImg && (
-            <>
-              <Denom
-                type={type}
-                denomValue={denom[0].denom}
-                onlyText
-                tooltipStatusImg={tooltipStatusImg}
-                tooltipStatusText={tooltipStatusText}
-                infoDenom={denom[0]}
-                size={size}
-              />
-              -
-              <Denom
-                type={type}
-                denomValue={denom[1].denom}
-                onlyText
-                tooltipStatusImg={tooltipStatusImg}
-                tooltipStatusText={tooltipStatusText}
-                infoDenom={denom[1]}
-                size={size}
-              />
-            </>
-          )}
-        </div>
-      );
-    }
-    return null;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [denomValue]);
-
-  try {
-    return useDenomValue;
-    // return <ValueImg text={denom} type={type} {...props} />;
-  } catch (error) {
-    return <div>{denomValue}</div>;
+  if (type === 'network') {
+    return (
+      <Denom
+        onlyImg={onlyImg}
+        onlyText={onlyText}
+        denomValue={denomValue}
+        tooltipStatusImg={tooltipStatusImg}
+        tooltipStatusText={tooltipStatusText}
+        type={type}
+        infoDenom={{}}
+        gap={13}
+        size={size}
+      />
+    );
   }
+
+  if (denom && denom.length === 1) {
+    return (
+      <Denom
+        onlyImg={onlyImg}
+        onlyText={onlyText}
+        denomValue={denom[0].denom}
+        tooltipStatusImg={tooltipStatusImg}
+        tooltipStatusText={tooltipStatusText}
+        type={type}
+        infoDenom={denom[0]}
+        gap={13}
+        size={size}
+      />
+    );
+  }
+
+  if (denom && denom.length > 1) {
+    return (
+      <div style={{ display: 'flex' }}>
+        {!onlyText && (
+          <>
+            <Denom
+              type={type}
+              denomValue={denom[0].denom}
+              onlyImg
+              tooltipStatusImg={tooltipStatusImg}
+              tooltipStatusText={tooltipStatusText}
+              infoDenom={denom[0]}
+              size={size}
+            />
+            <Denom
+              type={type}
+              denomValue={denom[1].denom}
+              marginContainer={
+                onlyImg ? '0px 0px 0px -12px' : '0px 5px 0px -12px'
+              }
+              onlyImg
+              tooltipStatusImg={tooltipStatusImg}
+              tooltipStatusText={tooltipStatusText}
+              infoDenom={denom[1]}
+              size={size}
+            />
+          </>
+        )}
+
+        {!onlyImg && (
+          <>
+            <Denom
+              type={type}
+              denomValue={denom[0].denom}
+              onlyText
+              tooltipStatusImg={tooltipStatusImg}
+              tooltipStatusText={tooltipStatusText}
+              infoDenom={denom[0]}
+              size={size}
+            />
+            -
+            <Denom
+              type={type}
+              denomValue={denom[1].denom}
+              onlyText
+              tooltipStatusImg={tooltipStatusImg}
+              tooltipStatusText={tooltipStatusText}
+              infoDenom={denom[1]}
+              size={size}
+            />
+          </>
+        )}
+      </div>
+    );
+  }
+  return null;
 }
 
 export default DenomArr;
