898934b85616b7b3c7df40afa67de5dad8d69cfd
dimakorzhovnik <korzhovnik.dima@gmail.com>
2023-12-14 16:43:34 +0300

    fix(sense): remove comments

--- src/containers/taverna/useGetTweets.ts
+++ src/containers/taverna/useGetTweets.ts
@@ -22,42 +22,7 @@ const useGetTweets = (addressActive: Option<string>) => {
   const [tweets, setTweets] = useState<TweetTemp>({});
   const [tweetData, setTweetData] = useState([]);
   const [loadingTweets, setLoadingTweets] = useState(false);
-  const [addressFollowData, setAddressFollowData] = useState({});
   const { fetchParticleAsync } = useQueueIpfsContent(addressActive);
-
-  // useEffect(() => {
-  //   if (!cyber?.connected) {
-  //     return;
-  //   }
-
-  //   const param = {
-  //     query:
-  //       `tm.event='Tx' AND message.action='link' AND cyberlink.objectFrom='${CID_TWEET}'`,
-  //   };
-
-  //   if (cyber.subscriptions.includes(JSON.stringify(param))) {
-  //     return;
-  //   }
-
-  //   cyber.sendMessage({
-  //     jsonrpc: '2.0',
-  //     method: 'subscribe',
-  //     id: '0',
-  //     params: param,
-  //   });
-  // }, [cyber, cyber?.connected]);
-
-  // useEffect(() => {
-  //   if (!cyber?.message?.result) {
-  //     return;
-  //   }
-
-  //   const { message } = cyber;
-
-  //   if (Object.keys(message.result).length > 0) {
-  //     updateWs(message.result.events);
-  //   }
-  // }, [cyber, cyber?.message]);
 
   const getFollow = useCallback(
     async (responseFollows) => {
@@ -69,11 +34,6 @@ const useGetTweets = (addressActive: Option<string>) => {
           : undefined;
 
         if (addressResolve && addressResolve.match(PATTERN_CYBER)) {
-          setAddressFollowData((itemState) => ({
-            ...itemState,
-            [addressResolve]: cid,
-          }));
-
           const responseTweet = await getTweet(addressResolve);
           if (
             responseTweet &&
@@ -140,35 +100,6 @@ const useGetTweets = (addressActive: Option<string>) => {
       setLoadingTweets(false);
     }
   }, [tweetData]);
-
-  // const updateWs = async (data) => {
-  //   const subject = data['cybermeta.subject']?.[0];
-  //   const objectTo = data['cyberlink.objectTo']?.[0];
-  //   const d = new Date();
-  //   const timestamp = dateFormat(d, 'isoUtcDateTime');
-
-  //   // TODO: filter by type
-  //   if (!subject || !objectTo) {
-  //     return;
-  //   }
-
-  //   if (
-  //     Object.keys(addressFollowData).length > 0 &&
-  //     Object.prototype.hasOwnProperty.call(addressFollowData, subject)
-  //   ) {
-  //     setTweets((item) => ({
-  //       [objectTo]: {
-  //         status: 'impossibleLoad',
-  //         text: objectTo,
-  //         address: subject,
-  //         content: false,
-  //         time: timestamp,
-  //         rank: null,
-  //       },
-  //       ...item,
-  //     }));
-  //   }
-  // };
 
   return {
     tweets,
