bb57f1dc2d2f1e74ebc8be6ea4e8c32378de1371
Cheslav Zhuravsky <happylolonly@gmail.com>
2024-02-03 19:21:07 +0400

    update sense button

--- src/features/sense/ui/SenseButton/SenseButton.tsx
+++ src/features/sense/ui/SenseButton/SenseButton.tsx
@@ -5,26 +5,31 @@ import cx from 'classnames';
 import { Tooltip } from 'src/components';
 import { useAppSelector } from 'src/redux/hooks';
 import { selectUnreadCounts } from '../../redux/sense.redux';
+import { Tooltip } from 'src/components';
 
 type Props = {
   className?: string;
 };
 
 function SenseButton({ className }: Props) {
-  const unreadCounts = useAppSelector(selectUnreadCounts);
+  const { particles, neurons } = useAppSelector(selectUnreadCounts);
+  const total = particles + neurons;
 
   return (
     <Link
       className={cx(styles.senseBtn, className)}
       to={routes.robot.routes.sense.path}
     >
-      <Tooltip tooltip="unread chats" placement="right">
+      {/* <Tooltip tooltip="unread chats" placement="right">
         <span>{unreadCounts.neurons}</span>
       </Tooltip>
       <Tooltip tooltip="cyberlinks' notifications" placement="right">
         <span>{unreadCounts.particles}</span>
-      </Tooltip>
+      </Tooltip> */}
 
+      <Tooltip tooltip="sense notifications">
+        <span>{total}</span>
+      </Tooltip>
       <span>all</span>
     </Link>
   );
