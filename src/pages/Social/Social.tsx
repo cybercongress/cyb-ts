a1a35785dec1849164412dc31f071e1a47676b16
dimakorzhovnik <korzhovnik.dima@gmail.com>
2024-02-04 03:04:34 +0300

    fix(social): hub links

--- src/pages/Social/Social.tsx
+++ src/pages/Social/Social.tsx
@@ -1,13 +1,15 @@
-import { MainContainer } from 'src/components';
+import { LinkWindow, MainContainer } from 'src/components';
 import Discord from 'src/pages/Social/Discord/Discord';
 import { GitHub } from 'src/pages/Social/GitHub/GitHub';
 import { Telegram } from 'src/pages/Social/Telegram/Telegram';
-import Twitter from './Twitter/Twitter';
 import Display from 'src/components/containerGradient/Display/Display';
 import DisplayTitle from 'src/components/containerGradient/DisplayTitle/DisplayTitle';
-import styles from './Social.module.scss';
 import { useAdviser } from 'src/features/adviser/context';
 import { useEffect } from 'react';
+import Twitter from './Twitter/Twitter';
+import styles from './Social.module.scss';
+
+const HUB_LINK = 'https://hackmd.io/J2P6k7ANT5GIXI3D1mPzrQ';
 
 // TODO: folder is dirty, can be refactored
 function Social() {
@@ -33,6 +35,13 @@ function Social() {
             <GitHub />
           </div>
 
+          <br />
+          <DisplayTitle inDisplay title="More links" />
+
+          <LinkWindow className={styles.hubLinks} to={HUB_LINK}>
+            <div>👾</div>
+            <span>Hub links</span>
+          </LinkWindow>
           <br />
           <DisplayTitle inDisplay title="other" />
           <a
