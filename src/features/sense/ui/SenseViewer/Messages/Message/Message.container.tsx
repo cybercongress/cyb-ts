db03fef679fc6f38094ef526990d11f98cb7ae94
Cheslav Zhuravsky <happylolonly@gmail.com>
2024-02-27 13:03:26 +0400

    resolve cid for text

--- src/features/sense/ui/SenseViewer/Messages/Message/Message.container.tsx
+++ src/features/sense/ui/SenseViewer/Messages/Message/Message.container.tsx
@@ -5,16 +5,18 @@ import Message from './Message';
 import useParticleDetails from '../../../../../particle/useParticleDetails';
 import ContentIpfs from 'src/components/contentIpfs/contentIpfs';
 import { Dots } from 'src/components';
-import { routes } from 'src/routes';
-import { Link } from 'react-router-dom';
 import { SenseItem } from 'src/features/sense/redux/sense.redux';
 import { formatSenseItemDataToUI } from '../../../utils/format';
+import { getIpfsHash } from 'src/utils/ipfs/helpers';
+import { useEffect, useState } from 'react';
 
 type Props = {
   senseItem: SenseItem;
 };
 
 function MessageContainer({ senseItem }: Props) {
+  const [textCid, setTextCid] = useState<string>();
+
   const address = useAppSelector(selectCurrentAddress);
 
   const {
@@ -31,37 +33,42 @@ function MessageContainer({ senseItem }: Props) {
     skip: Boolean(text && !cid),
   });
 
-  function renderCidContent() {
-    if (cid) {
-      if (loading) {
-        return (
-          <span>
-            resolving particle <Dots />
-          </span>
-        );
-        // eslint-disable-next-line no-else-return
-      } else if (data) {
-        return (
-          <>
-            <ContentIpfs
-              details={data}
-              cid={data.cid}
-              content={data.content}
-              search
-            />
-
-            {/* {data.type === 'text' && data.text?.endsWith('...') && (
-              <>
-                <br />
-                <Link to={routes.oracle.ask.getLink(cid)} target="_blank">
-                  full content
-                </Link>
-              </>
-            )} */}
-          </>
-        );
-      }
+  useEffect(() => {
+    if (!text) {
+      return;
     }
+
+    (async () => {
+      const cid = await getIpfsHash(text);
+
+      console.debug('text', text);
+      console.debug('cid', cid);
+
+      setTextCid(cid);
+    })();
+  }, [text]);
+
+  let content;
+
+  if (cid) {
+    if (loading) {
+      content = (
+        <span>
+          resolving particle <Dots />
+        </span>
+      );
+    } else if (data) {
+      content = (
+        <ContentIpfs
+          details={data}
+          cid={data.cid}
+          content={data.content}
+          search
+        />
+      );
+    }
+  } else if (text) {
+    content = text;
   }
 
   return (
@@ -69,8 +76,8 @@ function MessageContainer({ senseItem }: Props) {
       address={from}
       txHash={transactionHash}
       date={timestamp}
-      cid={cid}
-      content={cid ? renderCidContent() : text}
+      content={content}
+      cid={cid || textCid}
       amountData={{
         amount,
         isAmountSendToMyAddress,
