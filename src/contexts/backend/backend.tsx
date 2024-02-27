46234ec3a15e0b927a879986cc49a6351b45a2cb
Cheslav Zhuravsky <happylolonly@gmail.com>
2024-02-27 14:24:34 +0400

    remove call

--- src/contexts/backend/backend.tsx
+++ src/contexts/backend/backend.tsx
@@ -11,7 +11,6 @@ import { selectCurrentAddress } from 'src/redux/features/pocket';
 import DbApiWrapper from 'src/services/backend/services/dataSource/indexedDb/dbApiWrapper';
 import { CozoDbWorker } from 'src/services/backend/workers/db/worker';
 import { BackgroundWorker } from 'src/services/backend/workers/background/worker';
-import { updateSenseList } from 'src/features/sense/redux/sense.redux';
 import { SenseApi, createSenseApi } from './services/senseApi';
 import { SyncEntryName } from 'src/services/backend/types/services';
 import BroadcastChannelListener from 'src/services/backend/channels/BroadcastChannelListener';
@@ -134,15 +133,6 @@ function BackendProvider({ children }: { children: React.ReactNode }) {
     }
     return null;
   }, [isDbInitialized, dbApi, myAddress, followings]);
-
-  useEffect(() => {
-    if (senseApi) {
-      (async () => {
-        const list = await senseApi.getList();
-        dispatch(updateSenseList(list));
-      })();
-    }
-  }, [senseApi, dispatch]);
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
