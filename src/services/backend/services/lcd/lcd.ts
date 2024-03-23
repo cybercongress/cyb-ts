d956931a9ff359d9647f7a5081b5c8ec357ff942
dasein <acidpictures@gmail.com>
2024-03-23 22:18:16 +0530

    feat(lcd): remove

--- src/services/backend/services/lcd/lcd.ts
+++ src/services/backend/services/lcd/lcd.ts
@@ -3,33 +3,31 @@ import { NeuronAddress, ParticleCid } from 'src/types/base';
 import { CID_FOLLOW } from 'src/constants/app';
 import { getIpfsHash } from 'src/utils/ipfs/helpers';
 import { LCD_URL } from 'src/constants/config';
-import { Api } from 'src/generated/lcd';
+// import { Api } from 'src/generated/lcd';
 
-const lcdApi = new Api({ baseURL: LCD_URL });
+// const lcdApi = new Api({ baseURL: LCD_URL });
 
 export const getFollowsAsCid = async (
   address: NeuronAddress,
   signal?: AbortSignal
 ): Promise<ParticleCid[]> => {
-  const response = await lcdApi.cosmos.getTxsEvent(
-    {
-      events: [
-        `cyberlink.neuron=${address}`,
-        `cyberlink.particleFrom=${CID_FOLLOW}`,
-      ],
-      paginationLimit: '1000000000',
-    },
-    { signal }
-  );
+  // const response = await lcdApi.cosmos.getTxsEvent(
+  //   {
+  //     events: [
+  //       `cyberlink.neuron=${address}`,
+  //       `cyberlink.particleFrom=${CID_FOLLOW}`,
+  //     ],
+  //     paginationLimit: '1000000000',
+  //   },
+  //   { signal }
+  // );
 
-  // const response = await axios({
-  //   method: 'get',
-  //   url: `${LCD_URL}/txs?cyberlink.neuron=${address}&cyberlink.particleFrom=${CID_FOLLOW}&limit=1000000000`,
-  //   signal,
-  // });
-  response.data.tx_responses!.map(
-    (item) => item.tx!.value!.msg![0].value!.links![0].to
-  );
+  const response = await axios({
+    method: 'get',
+    url: `${LCD_URL}/txs?cyberlink.neuron=${address}&cyberlink.particleFrom=${CID_FOLLOW}&limit=1000000000`,
+    signal,
+  });
+
   if (!response.data.txs) {
     return [];
   }
