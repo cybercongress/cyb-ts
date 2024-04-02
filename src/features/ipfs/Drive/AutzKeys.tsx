ead55d2eeb4fee72a647854e37e093b35a489d97
dasein <acidpictures@gmail.com>
2024-04-02 15:48:06 +0530

    feat(autz): grant cyberlink rights test

--- src/features/ipfs/Drive/AutzKeys.tsx
+++ src/features/ipfs/Drive/AutzKeys.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useAppSelector } from 'src/redux/hooks';
 import { Colors } from 'src/components/containerGradient/types';
 
 import Display from 'src/components/containerGradient/Display/Display';
 
 import styles from './drive.scss';
-import { Button } from 'src/components';
+import { Button, Input } from 'src/components';
 import { generateWalletFromMnemonic } from 'src/services/wallet/walletGenerator';
 import {
   createSignerClient,
@@ -19,11 +19,11 @@ import { GenericAuthorization } from 'cosmjs-types/cosmos/authz/v1beta1/authz';
 import { defaultFee } from 'src/services/neuron/neuronApi';
 import { makeClientWithAuthz } from './queries';
 import { coins } from '@cosmjs/launchpad';
-import BigNumber from 'bignumber.js';
-import Soft3MessageFactory from 'src/soft.js/api/msgs';
 import axios from 'axios';
 import { useBackend } from 'src/contexts/backend/backend';
 import { SigningCyberClient } from '@cybercongress/cyber-js';
+import { MsgCyberlink } from '@cybercongress/cyber-js/build/codec/cyber/graph/v1beta1/tx';
+import { MsgExec } from 'cosmjs-types/cosmos/authz/v1beta1/tx';
 
 /*
 https://lcd.bostrom.cybernode.ai/cosmos/authz/v1beta1/grants/grantee/bostrom1a3zu6emqfzkc6rz9a44mq2ks88dmgvjrfcq6jl
@@ -32,14 +32,14 @@ https://lcd.bostrom.cybernode.ai/cosmos/authz/v1beta1/grants/granter/bostrom1p0r
 
 https://lcd.bostrom.cybernode.ai/cosmos/authz/v1beta1/grants?granter=bostrom1p0r7uxstcw8ehrwuj4kn8qzzs0yypsjwxgd445&grantee=bostrom1a3zu6emqfzkc6rz9a44mq2ks88dmgvjrfcq6jl
 */
-const getGrantee = async (address: NeuronAddress) => {
+const getGranters = async (address: NeuronAddress) => {
   return axios({
     method: 'get',
     url: `${CYBER.CYBER_NODE_URL_LCD}/cosmos/authz/v1beta1/grants/grantee/${address}`,
   });
 };
 
-const getGranter = async (address: NeuronAddress) => {
+const getGrantees = async (address: NeuronAddress) => {
   return axios({
     method: 'get',
     url: `${CYBER.CYBER_NODE_URL_LCD}/cosmos/authz/v1beta1/grants/granter/${address}`,
@@ -59,14 +59,21 @@ function AutzKeys() {
   const { ipfsApi, senseApi } = useBackend();
   const [address, setAddress] = React.useState<string | null>(null);
   const [grantees, setGrantees] = React.useState<string[]>([]);
-  const [granters, setGranters] = React.useState<string[]>([]);
+  const [testLink, setTestLink] = React.useState<string>(
+    new Date().toISOString()
+  );
   const [autzSigner, setAutzSigner] = useState<
     SigningCyberClient | undefined
   >();
 
-  const [grants, setGrants] = React.useState<string[]>([]);
+  // const [grants, setGrants] = React.useState<string[]>([]);
   const { signingClient, signer, signerReady } = useSigningClient();
   const myAddress = useAppSelector(selectCurrentAddress);
+  const loadGrantees = useCallback(async () => {
+    const grantees = await getGrantees(myAddress);
+    setGrantees(grantees.data.grants);
+    console.log('----grantees', grantees.data.grants);
+  }, [myAddress]);
 
   useEffect(() => {
     (async () => {
@@ -76,17 +83,14 @@ function AutzKeys() {
         setAutzSigner(autzSigner);
 
         setAddress(address);
-        const grantees = await getGrantee(myAddress);
-        console.log('----grantees', grantees.data.grants);
-        setGrantees(grantees.data.grants);
+        await loadGrantees();
+        // const granters = await getGranters(myAddress);
+        // console.log('----grantees', granters.data.grants);
+        // setGranters(granters.data.grants);
 
-        const granters = await getGranter(myAddress);
-        console.log('----granters', granters.data.grants);
-        setGranters(granters.data.grants);
-
-        const grants = await getGrants(myAddress, address);
-        console.log('----grants', grants.data.grants);
-        setGrants(grants.data.grants);
+        // const grants = await getGrants(myAddress, address);
+        // console.log('----grants', grants.data.grants);
+        // setGrants(grants.data.grants);
       }
     })();
   }, [myAddress]);
@@ -101,11 +105,24 @@ function AutzKeys() {
           amount: coins(1, 'boot'),
         },
       };
+      // const thirtyDaysInFuture = new Date(
+      //   Date.now() + 30 * 24 * 60 * 60 * 1000
+      // );
+      // const expirationTimestamp = toTimestamp(thirtyDaysInFuture);
 
-      console.log('----onGrantClick sendMsg', sendMsg);
+      // const seconds = BigInt(
+      //   Math.round(thirtyDaysInFuture.getTime()).toString()
+      // );
+
+      // Create the Timestamp object with bigint values
+      // const expirationTimestamp = Timestamp.fromPartial({
+      //   seconds,
+      //   nanos: 0,
+      // });
+      const expirationTimestamp = Math.floor(
+        Date.parse('2024-05-01T18:52:56.449Z') / 1000
+      );
       const grantedMsg = '/cyber.graph.v1beta1.MsgCyberlink';
-      // const grantedMsg =
-      // '/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward';
 
       const grant = {
         authorization: {
@@ -116,6 +133,7 @@ function AutzKeys() {
             })
           ).finish(),
         },
+        expiration: { seconds: expirationTimestamp, nano: 0 },
       };
 
       const grantMsg = {
@@ -132,43 +150,50 @@ function AutzKeys() {
         [sendMsg, grantMsg],
         defaultFee
       );
-
+      await loadGrantees();
       console.log('----onGrantClick', result);
     }
   };
 
-  const onRevokeClick = () => {
-    console.log('----onRevokeClick');
+  const onRevokeClick = async () => {
+    console.log('-----revoke', grantees);
+    const result = await Promise.all(
+      grantees.map(
+        async (grant) =>
+          grant.grantee === address &&
+          signingClient!.revoke(
+            myAddress,
+            address,
+            grant.authorization.msg,
+            defaultFee
+          )
+      )
+    );
+    console.log('----onRevokeClick', result);
+
+    await loadGrantees();
   };
 
   const onTestLink = async () => {
     if (myAddress && address) {
-      // const deps = {
-      //   senseApi,
-      //   signingClient: signingClient!,
-      // };
       const fromCid = await ipfsApi?.addContent('test-autz');
-      const testContent = new Date().toISOString();
-      const toCid = await ipfsApi?.addContent(testContent);
+
+      const toCid = await ipfsApi?.addContent(testLink);
       console.log('----test link', fromCid, toCid);
-      // const res = await autzSigner?.cyberlink(
-      //   myAddress,
-      //   fromCid!,
-      //   toCid!,
-      //   defaultFee
-      // );
 
       const actionMsg = {
         typeUrl: '/cyber.graph.v1beta1.MsgCyberlink', // For example, sending tokens
-        value: {
-          neuron: myAddress,
-          links: [
-            {
-              from: fromCid,
-              to: toCid,
-            },
-          ],
-        },
+        value: MsgCyberlink.encode(
+          MsgCyberlink.fromPartial({
+            neuron: myAddress,
+            links: [
+              {
+                from: fromCid,
+                to: toCid,
+              },
+            ],
+          })
+        ).finish(),
       };
 
       // Step 3: Wrap the action message in a MsgExec message
@@ -179,14 +204,12 @@ function AutzKeys() {
           msgs: [actionMsg], // The message to be executed on behalf of the granter
         },
       };
-      const multiplier = new BigNumber(2).multipliedBy(2);
 
-      const fee = Soft3MessageFactory.fee(multiplier.toNumber());
       // Step 4: Sign and broadcast the MsgExec transaction
       const execResult = await autzSigner!.signAndBroadcast(
         address, // The grantee signs the transaction
         [execMsg], // The MsgExec message
-        fee, // Fee for the transaction
+        defaultFee, // Fee for the transaction
         'Executing action on behalf of granter 2w2' // Memo
       );
       console.log('----test link result', execResult, execMsg);
@@ -200,7 +223,7 @@ function AutzKeys() {
         <h3>Autz keys</h3>
         <div>Mnemonic: {mnemonic}</div>
         <div>Address: {address}</div>
-        <div>Granters:</div>
+        {/* <div>Granters:</div>
         <ul>
           {granters.map((data, index) => (
             <li key={`grantees_${index}`}>
@@ -208,7 +231,7 @@ function AutzKeys() {
               {data.expiration})
             </li>
           ))}
-        </ul>
+        </ul> */}
         <div>Grantees:</div>
         <ul>
           {grantees.map((data, index) => (
@@ -218,23 +241,31 @@ function AutzKeys() {
             </li>
           ))}
         </ul>
-        <div>Grants:</div>
+        {/* <div>Grants:</div>
         <ul>
           {grants.map((data, index) => (
             <li key={`grant_${index}`}>
               {data.authorization.msg}: ({data.expiration})
             </li>
           ))}
-        </ul>
+        </ul> */}
         <div className={styles.buttonPanel}>
           <Button small onClick={onGrantClick}>
             grant link rights
-          </Button>{' '}
+          </Button>
+          <span> </span>
           <Button small onClick={onRevokeClick}>
             revoke rights
           </Button>{' '}
+        </div>
+        <div className={styles.buttonPanel}>
+          <Input
+            value={testLink}
+            onChange={(v) => setTestLink(v.target.value)}
+            placeholder="any text"
+          />
           <Button small onClick={onTestLink}>
-            test autz link
+            link to test-autz
           </Button>
         </div>
       </div>
