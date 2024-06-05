c15741ef1ba7a9acefcd47b5a8b5d51bf0e7b0d7
Cheslav Zhuravsky <happylolonly@gmail.com>
2024-06-05 14:00:16 +0700

    fix

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -12,9 +12,9 @@ function isWorker() {
 const LOCALSTORAGE_CHAIN_ID = !isWorker() && localStorage.getItem('chainId');
 
 const DEFAULT_CHAIN_ID: Networks.BOSTROM | Networks.SPACE_PUSSY =
-  LOCALSTORAGE_CHAIN_ID || Networks.BOSTROM;
+  LOCALSTORAGE_CHAIN_ID || process.env.CHAIN_ID || Networks.BOSTROM;
 
-export const CHAIN_ID = process.env.CHAIN_ID || DEFAULT_CHAIN_ID;
+export const CHAIN_ID = DEFAULT_CHAIN_ID;
 
 export const LCD_URL =
   process.env.LCD_URL || defaultNetworks[DEFAULT_CHAIN_ID].LCD_URL;
