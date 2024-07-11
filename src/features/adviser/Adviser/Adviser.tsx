cd954caba477ddf81546847a9d6620841e2571c4
Cheslav Zhuravsky <happylolonly@gmail.com>
2024-07-11 22:01:41 +0700

    add comment

--- src/features/adviser/Adviser/Adviser.tsx
+++ src/features/adviser/Adviser/Adviser.tsx
@@ -28,6 +28,7 @@ function prepareText(text: string) {
   t = t.replace(/\n/g, '. ');
 
   // replace emoji
+  // TODO: RGI_Emoji can be used in ECMAScript 2024 soon
   t = t.replace(
     /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu,
     ''
