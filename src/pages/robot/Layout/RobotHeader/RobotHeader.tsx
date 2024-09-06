804390f85aff011e7d353d12b37698ab46ac0831
OggyKUN <dopnub@gmail.com>
2024-09-06 14:26:18 +0300

    fix(robot): header swap info

--- src/pages/robot/Layout/RobotHeader/RobotHeader.tsx
+++ src/pages/robot/Layout/RobotHeader/RobotHeader.tsx
@@ -9,23 +9,20 @@ import TabsNotOwner from './ui/TabsNotOwner/TabsNotOwner';
 
 function RobotHeader({ menuCounts }) {
   const { address, passport, isOwner } = useRobotContext();
-
-  const avatar = passport?.extension?.avatar;
-  const nickname = passport?.extension?.nickname;
+  const { avatar, nickname } = passport?.extension || {};
 
   return (
     <header className={styles.wrapper}>
       <Display noPadding color="blue">
         <div className={styles.content}>
           <div className={styles.level}>
+            {nickname && <h3 className={styles.name}>{nickname}</h3>}
             <Level value={menuCounts} />
-            {address && <FirstTx address={address} />}
           </div>
           <AvataImgIpfs addressCyber={address} cidAvatar={avatar} />
-
           <div className={styles.addressName}>
-            {nickname && <h3 className={styles.name}>{nickname}</h3>}
             <MusicalAddress address={address} />
+            <FirstTx address={address} />
           </div>
         </div>
         {!isOwner && <TabsNotOwner menuCounts={menuCounts} />}
