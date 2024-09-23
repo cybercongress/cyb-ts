e1ec76ba8a03e41b2c0ae861861baa6cf1998ad6
OggyKUN <dopnub@gmail.com>
2024-09-23 19:39:31 +0300

    feat(robotheader): add hover on lvl with its description

--- src/pages/robot/Layout/RobotHeader/RobotHeader.tsx
+++ src/pages/robot/Layout/RobotHeader/RobotHeader.tsx
@@ -6,6 +6,7 @@ import { useRobotContext } from '../../robot.context';
 import FirstTx from './ui/FirstTx/FirstTx';
 import Level from './ui/Level/Level';
 import TabsNotOwner from './ui/TabsNotOwner/TabsNotOwner';
+import AdviserHoverWrapper from 'src/features/adviser/AdviserHoverWrapper/AdviserHoverWrapper';
 
 function RobotHeader({ menuCounts }) {
   const { address, passport, isOwner } = useRobotContext();
@@ -17,7 +18,10 @@ function RobotHeader({ menuCounts }) {
         <div className={styles.content}>
           <div className={styles.level}>
             {nickname && <h3 className={styles.name}>{nickname}.moon</h3>}
-            <Level value={menuCounts} />
+
+            <AdviserHoverWrapper adviserContent="the level increases based on your stats, such as sigma, karma, brain, and others">
+              <Level value={menuCounts} />
+            </AdviserHoverWrapper>
           </div>
           <AvataImgIpfs addressCyber={address} cidAvatar={avatar} />
           <div className={styles.addressName}>
