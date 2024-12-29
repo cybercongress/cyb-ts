2bdcab4ab1025bc7c7d33dd75790e08ac4c81969
Cheslav Zhuravsky <happylolonly@gmail.com>
2024-12-29 15:44:14 +0700

    fix

--- src/containers/Search/LLMSpark/index.ts
+++ src/containers/Search/LLMSpark/index.ts
@@ -1,4 +1,4 @@
-const var1 = 'NzFlZjJiOGVlZDRlNDYyOWNjZDY5YzQzZDljMjEwMzJhZGJhYmI5MGI1ZW';
-const var2 = 'ZiMzBlZmVjMmRhZDE5MjNiODFmNQ';
+const var1 = 'YWFjNzQ5OTRlMmFjZWY0ZGU2MGZjNDZkY2FiMjQxYWJjYm';
+const var2 = 'Y5NDdlODE3ZjhhNzI2OWFlMjUzYTg1MjliZmI3MQ==';
 
 export const testVar = var1 + var2;
